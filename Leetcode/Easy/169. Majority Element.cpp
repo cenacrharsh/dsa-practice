7cecd24632d128c0274fb7fb4d9992c5b6de1211
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-02-15 17:28:57 +0530

    majority element with moore voting algorithm

--- Leetcode/Easy/169. Majority Element.cpp
+++ Leetcode/Easy/169. Majority Element.cpp
@@ -1,12 +1,79 @@
+//# Tutorial: https://www.youtube.com/watch?v=AoX3BPWNnoE
+
 #include <bits/stdc++.h>
 #include <vector>
 #include <algorithm>
 #include <climits>
 using namespace std;
 
+/*
+> Moore Voting Algorithm
+* If the Majority element was x, and it by any chance appears on the left prefix, then it count at max can be (lenOfLeftPrefix / 2). So since it has to appear more than N/2 times, it is bound to appear as a majority in the last suffix.
+
+# prefix is the left part of array at which point our count = 0, i.e. n(majority) = n(minority) upto this point, hence since majority has to appear > n/2 times, it becomes majority the right parefix or leftover part of array
+*/
+
+class Solution
+{
+public:
+ int majorityElement(vector<int> &nums)
+ {
+  int count = 0;
+  int candidate = 0;
+  for (auto num : nums)
+  {
+   if (count == 0)
+   {
+    candidate = num;
+   }
+
+   if (num == candidate)
+   {
+    count++;
+   }
+   else
+   {
+    count--;
+   }
+  }
+  return candidate;
+ }
+};
+
 //! Approach 1 - Moore's Voting Algo
 
-//! Approach 2
+//! Approach 2 - Hasmap
+
+/*
+> Time Complexity: O(n)/o(n * log n)
+> Space Complexity: O(n)
+*/
+
+#include <unordered_map>
+class Solution
+{
+public:
+ int majorityElement(vector<int> &nums)
+ {
+  unordered_map<int, int> freq;
+  int requiredCount = nums.size() / 2;
+  int ans;
+  for (int i = 0; i < nums.size(); i++)
+  {
+   freq[nums[i]]++;
+  }
+  for (auto f : freq)
+  {
+   if (f.second > requiredCount)
+   {
+    ans = f.first;
+   }
+  }
+  return ans;
+ }
+};
+
+//! Approach 3
 
 class Solution
 {
