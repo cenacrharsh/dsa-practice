a61f67837dde58a7e8f3bdd121198739ebbe8bb1
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-02-16 13:33:15 +0530

    Find the special index of array

---
+++ Supercoder Codequotient DSA/Week 3/Find the special index of array.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+using namespace std;
+
+/*
+ * Complete the 'specialIndex' function below.
+ * @params
+ * arr -> An vector of integers
+ *
+ * @return
+ * The index of an array such that the sum of elements at
+ * lower indices is equal to the sum of elements at higher indices.
+ */
+
+int specialIndex(vector<int> arr)
+{
+ int n = arr.size();
+ int prefixSum[n], suffixSum[n];
+
+ //! prefixSum contains sum of elements till i - 1
+ prefixSum[0] = 0;
+ for (int i = 1; i < n; i++)
+ {
+  prefixSum[i] = prefixSum[i - 1] + arr[i - 1];
+ }
+
+ //! suffixSum contains sum of elements from i + 1
+ suffixSum[n - 1] = 0;
+ for (int i = n - 2; i >= 0; i--)
+ {
+  suffixSum[i] = suffixSum[i + 1] + arr[i + 1];
+ }
+
+ /*
+ for(int i=0;i<n;i++)
+ {
+   cout << "prefixSum[i]: " << prefixSum[i] << " suffixSum[i]: " << suffixSum[i] << endl;
+ }
+ */
+
+ int ans = -1;
+ for (int i = 0; i < n; i++)
+ {
+  if (prefixSum[i] == suffixSum[i])
+  {
+   ans = i;
+   break;
+  }
+ }
+ return ans;
+}
