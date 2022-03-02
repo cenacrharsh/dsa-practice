73f49f6b640d5c6a59546f87b1b2716a4230a6d7
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-03 00:11:44 +0530

    151. Reverse Words in a String

---
+++ Leetcode/Medium/151. Reverse Words in a String.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<algorithm>
+#include<climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res="";
+        vector<string> rev;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] != ' ')
+            {
+                res += s[i];
+            }
+            else
+            {
+                if(!res.empty())
+                {
+                    rev.push_back(res);   
+                }
+                res = "";
+            }
+        }
+        if(!res.empty())
+        {
+            rev.push_back(res);   
+        }
+        res = "";
+        for(int i = rev.size() - 1; i >= 0; i--)
+        {
+            res = res + rev[i] + " ";
+        }
+        res.pop_back();
+        return res;
+    }
+};
