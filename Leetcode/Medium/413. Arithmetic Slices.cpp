91da3266aa8c107b90067855e438eeb47235a775
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-03 23:47:50 +0530

    413. Arithmetic Slices

---
+++ Leetcode/Medium/413. Arithmetic Slices.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Solution
+{
+public:
+ int numberOfArithmeticSlices(vector<int> &nums)
+ {
+  int n = nums.size();
+  if (n < 3)
+  {
+   return 0;
+  }
+  vector<int> dp(n, 0);
+  int count = 0;
+  for (int i = 2; i < n; i++)
+  {
+   if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2])
+   {
+    dp[i] = dp[i - 1] + 1;
+    count += dp[i];
+   }
+  }
+  return count;
+ }
+};
