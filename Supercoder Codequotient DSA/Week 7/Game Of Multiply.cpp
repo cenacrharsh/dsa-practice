b128367a89dfe6746f1e3d1244e0f608fa1ced34
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-17 23:41:41 +0530

    Game Of Multiply

---
+++ Supercoder Codequotient DSA/Week 7/Game Of Multiply.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+/*
+after n multiplies:
+even -> +ve
+odd -> -ve
+
+therefore from [1, n] those no. which have odd no. of factors ===
+all the perfect sq in [1, n] === int(sqRoot(n))
+*/
+#include <cmath>
+int multiplyGame(int N)
+{
+ // Write your code here
+ int ans = sqrt(N);
+ return ans;
+}
