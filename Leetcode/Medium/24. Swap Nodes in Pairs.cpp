2db990ac0d331b9d7da39c65b7a1ead06ad09e66
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-19 00:48:53 +0530

    24. Swap Nodes in Pairs

---
+++ Leetcode/Medium/24. Swap Nodes in Pairs.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+/*
+> Time Complexity: O(n)
+> Space Complexity: O(1)
+*/
+
+struct ListNode
+{
+ int val;
+ ListNode *next;
+ ListNode() : val(0), next(nullptr) {}
+ ListNode(int x) : val(x), next(nullptr) {}
+ ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+ ListNode *swapPairs(ListNode *head)
+ {
+  if (head == NULL)
+  {
+   return head;
+  }
+
+  ListNode *temp = new ListNode();
+  temp->next = head;
+  ListNode *current = temp;
+
+  /*
+   ! 1 -> 2 -> 3 -> 4 =>
+   ! 2 -> 1 -> 3 -> 4 =>
+   ! 2 -> 1 -> 4 -> 3
+  */
+
+  while (current->next != NULL && current->next->next != NULL)
+  {
+   ListNode *first = current->next;
+   ListNode *second = current->next->next;
+
+   first->next = second->next;  // 1 -> 3, 3 -> NULL
+   current->next = second;      // head -> 2, 1 -> 4
+   current->next->next = first; // 2 -> 1, 4 -> 3
+
+   current = current->next->next; // head -> 1
+  }
+  return temp->next;
+ }
+};
