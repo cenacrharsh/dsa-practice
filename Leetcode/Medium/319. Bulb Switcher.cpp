5fdbb961c8c9e9793c5672066155165c810c8bf7
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-18 10:24:32 +0530

    319. Bulb Switcher

---
+++ Leetcode/Medium/319. Bulb Switcher.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Solution
+{
+public:
+ int bulbSwitch(int n)
+ {
+  return sqrt(n);
+ }
+};
