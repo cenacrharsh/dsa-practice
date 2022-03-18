bf76252b33e60e823ede064424327769c3341753
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-18 14:06:04 +0530

    1051. Height Checker

---
+++ Leetcode/Easy/1051. Height Checker.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Solution
+{
+public:
+ int heightChecker(vector<int> &heights)
+ {
+  vector<int> temp = heights;
+  sort(temp.begin(), temp.end());
+  int count = 0;
+  for (int i = 0; i < heights.size(); i++)
+  {
+   if (heights[i] != temp[i])
+   {
+    count++;
+   }
+  }
+  return count;
+ }
+};
