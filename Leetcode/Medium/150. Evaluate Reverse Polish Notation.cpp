c795a2778e03cfd7bb4504de2977fba330091b40
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-22 22:24:51 +0530

    leetcode 150

---
+++ Leetcode/Medium/150. Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Solution
+{
+public:
+ int evalRPN(vector<string> &tokens)
+ {
+  stack<int> st;
+
+  for (int i = 0; i < tokens.size(); i++)
+  {
+   if (tokens[i] == "+" or tokens[i] == "-" or tokens[i] == "*" or tokens[i] == "/")
+   {
+    int op2 = st.top();
+    st.pop();
+    int op1 = st.top();
+    st.pop();
+
+    if (tokens[i] == "+")
+    {
+     st.push(op1 + op2);
+    }
+    else if (tokens[i] == "-")
+    {
+     st.push(op1 - op2);
+    }
+
+    else if (tokens[i] == "*")
+    {
+     st.push(op1 * op2);
+    }
+
+    else if (tokens[i] == "/")
+    {
+     st.push(op1 / op2);
+    }
+   }
+   else
+   {
+    int ele = stoi(tokens[i]);
+    st.push(ele);
+   }
+  }
+
+  return st.top();
+ }
+};
