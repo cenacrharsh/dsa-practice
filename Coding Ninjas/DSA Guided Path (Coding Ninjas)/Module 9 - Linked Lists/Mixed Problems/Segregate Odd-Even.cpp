810e90d047f3a3d0ab7efe99eeda6f04e8ad0900
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-24 14:36:39 +0530

    Segregate Odd-Even

---
+++ Coding Ninjas/DSA Guided Path (Coding Ninjas)/Module 9 - Linked Lists/Mixed Problems/Segregate Odd-Even.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Node
+{
+public:
+ int data;
+ Node *next;
+ Node(int data)
+ {
+  this->data = data;
+  this->next = NULL;
+ }
+};
+
+/*
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+
+    Where N is the number of the nodes in the linked list
+*/
+
+Node *segregateOddEven(Node *head)
+{
+ Node *oddStart = NULL, *oddEnd = NULL, *evenStart = NULL, *evenEnd = NULL;
+
+ Node *curr = head;
+
+ while (curr != NULL)
+ {
+  // If the node is odd, add it to the odd linked list.
+  if (curr->data % 2 == 1)
+  {
+   if (oddStart == NULL)
+   {
+    oddStart = curr;
+    oddEnd = oddStart;
+   }
+   else
+   {
+    oddEnd->next = curr;
+    oddEnd = oddEnd->next;
+   }
+  }
+  // If the node is even, add it to the even linked list.
+  else
+  {
+   if (evenStart == NULL)
+   {
+    evenStart = curr;
+    evenEnd = evenStart;
+   }
+   else
+   {
+    evenEnd->next = curr;
+    evenEnd = evenEnd->next;
+   }
+  }
+
+  // Update current for the next iteration.
+  curr = curr->next;
+ }
+
+ /*
+  If either odd list or even list is empty,
+     no change is required as all elements
+     are either even or odd.
+    */
+ if (oddStart == NULL or evenStart == NULL)
+ {
+  return head;
+ }
+
+ // Connect even linked list after odd.
+ oddEnd->next = evenStart;
+ evenEnd->next = NULL;
+
+ // Return the head of the modified list i.e oddStart.
+ return oddStart;
+}
