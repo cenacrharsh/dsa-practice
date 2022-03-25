b0f7fd55eb10f91a611fb71b6297d7164e111065
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-25 23:54:38 +0530

    496. Next Greater Element I

---
+++ Leetcode/Easy/496. Next Greater Element I.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Solution
+{
+public:
+ vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+ {
+  unordered_map<int, int> map;
+  stack<int> st;
+  st.push(nums2[0]);
+  for (int i = 1; i < nums2.size(); i++)
+  {
+   if (!st.empty() && nums2[i] > st.top())
+   {
+    while (!st.empty() && nums2[i] > st.top())
+    {
+     map[st.top()] = nums2[i];
+     st.pop();
+    }
+   }
+   st.push(nums2[i]);
+  }
+  while (!st.empty())
+  {
+   map[st.top()] = -1;
+   st.pop();
+  }
+  vector<int> ans;
+  for (int i = 0; i < nums1.size(); i++)
+  {
+   ans.push_back(map[nums1[i]]);
+  }
+  return ans;
+ }
+};
