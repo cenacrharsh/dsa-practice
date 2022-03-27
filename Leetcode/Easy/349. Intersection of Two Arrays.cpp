2061d1c8b2940e35e0578cce644c034ccc7ad8f5
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-27 11:35:14 +0530

    349. Intersection of Two Arrays

---
+++ Leetcode/Easy/349. Intersection of Two Arrays.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Solution
+{
+public:
+ vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+ {
+  unordered_set<int> set1(begin(nums1), end(nums1)), set2(begin(nums2), end(nums2));
+  vector<int> ans;
+  for (auto i : set1)
+  {
+   if (set2.find(i) != set2.end())
+   {
+    ans.push_back(i);
+   }
+  }
+  return ans;
+ }
+};
