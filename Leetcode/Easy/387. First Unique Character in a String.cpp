01be0bd7c99f248628a3944cb8014411d0700855
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-28 18:48:43 +0530

    387. First Unique Character in a String

--- Leetcode/Easy/387. First Unique Character in a String.cpp
+++ Leetcode/Easy/387. First Unique Character in a String.cpp
@@ -6,7 +6,40 @@
 #include <cstring>
 using namespace std;
 
+//! Using Freq Array
+
+/*
+> Time Complexity: O(n)
+> Space Complexity: O(1)
+*/
+
+class Solution
+{
+public:
+ int firstUniqChar(string s)
+ {
+  vector<int> freq(26);
+  for (int i = 0; i < s.size(); i++)
+  {
+   freq[s[i] - 'a']++;
+  }
+  for (int i = 0; i < s.size(); i++)
+  {
+   if (freq[s[i] - 'a'] == 1)
+   {
+    return i;
+   }
+  }
+  return -1;
+ }
+};
+
 //! Using Unordered Map
+
+/*
+> Time Complexity: O(n)
+> Space Complexity: O(n)
+*/
 
 class Solution
 {
