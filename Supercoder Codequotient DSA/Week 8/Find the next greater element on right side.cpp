99398067fb21a2dd75dfc60041b560a7e3e6f855
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-29 14:41:14 +0530

    Find the next greater element on right side

--- Supercoder Codequotient DSA/Week 8/Find the next greater element on right side.cpp
+++ Supercoder Codequotient DSA/Week 8/Find the next greater element on right side.cpp
@@ -6,6 +6,62 @@
 #include <cstring>
 using namespace std;
 
+/*
+
+    Time Complexity - O(N)
+    Space Complexity - O(N)
+
+    Where 'N' is the number of elements in the array/list.
+
+*/
+
+void printNextGreaterElement(int arr[], int n)
+{
+ // Write your code here
+ stack<int> st;
+ vector<int> ans(n);
+
+ // Stack, to keep track of next greater element.
+ stack<int> s;
+
+ // Traverse in reverse fashion in the array.
+ for (int i = n - 1; i >= 0; i--)
+ {
+
+  /*
+      If current element is greater than stack's top
+      element, we can pop it since now it will never get
+      picked as the next greater, because current element
+      is greater and appear earlier.
+  */
+  while (!s.empty() && arr[i] >= s.top())
+  {
+   s.pop();
+  }
+
+  /*
+      If stack is not empty, stack's top element is
+      the next greater element, else there is none.
+  */
+  if (!s.empty())
+  {
+   ans[i] = s.top();
+  }
+  else
+  {
+   ans[i] = -1;
+  }
+
+  // Push current element onto stack.
+  s.push(arr[i]);
+ }
+
+ for (auto i : ans)
+ {
+  cout << i << " ";
+ }
+}
+
 void printNextGreaterElement(int arr[], int n)
 {
  // Write your code here
