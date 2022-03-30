a2fac8ebf303e9906b36bd095cd5f367237759ab
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-30 13:10:40 +0530

    Can we transform the two numbers

---
+++ Supercoder Codequotient DSA/Week 9/Can we transform the two numbers.cpp
@@ -0,0 +1,42 @@
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+//! Recusion
+
+/*
+ * Complete the 'canTransformed' function below.
+ * @params
+ *   a,b,c,d -> integers denoting the values of a,b,c,d respectively
+ * @return
+ *   1 if the numbers can be transformed , else 0
+ */
+
+int canTransformed(int a, int b, int c, int d)
+{
+ // Write your code here
+ if (a > c || b > d)
+ {
+  return 0;
+ }
+
+ if (a == c && b == d)
+ {
+  return 1;
+ }
+
+ int sum = a + b;
+ if (canTransformed(sum, b, c, d))
+ {
+  return 1;
+ }
+ else
+ {
+  return canTransformed(a, sum, c, d);
+ }
+}
