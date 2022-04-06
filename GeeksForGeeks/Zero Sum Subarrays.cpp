bfa3ab8195b5f8fec323798f3ac2c8d6c46b735d
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-04-06 14:13:16 +0530

    Zero Sum Subarrays

--- GeeksForGeeks/Zero Sum Subarrays.cpp
+++ GeeksForGeeks/Zero Sum Subarrays.cpp
@@ -8,6 +8,35 @@ using namespace std;
 
 #define ll long long
 
+//# Tutorial: https://www.youtube.com/watch?v=C9-n_H7dsvU
+
+class Solution
+{
+public:
+ // Function to count subarrays with sum equal to 0.
+ ll findSubarray(vector<ll> arr, int n)
+ {
+  // code here
+  unordered_map<ll, ll> prefixSumMap; // sum -> index
+  int i = -1;
+  ll sum = 0;
+  ll count = 0;
+  prefixSumMap[0] = 1;
+  while (i < n - 1)
+  {
+   i++;
+   sum += arr[i];
+   if (prefixSumMap.find(sum) != prefixSumMap.end())
+   {
+    count += prefixSumMap[sum];
+   }
+
+   prefixSumMap[sum]++;
+  }
+  return count;
+ }
+};
+
 class Solution
 {
 public:
