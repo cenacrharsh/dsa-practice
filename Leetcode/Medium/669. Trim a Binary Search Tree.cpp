5c7cac826f82c8ecd3d8fc06fcf9cfe1b1ce2889
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-04-16 00:03:01 +0530

    669. Trim a Binary Search Tree

---
+++ Leetcode/Medium/669. Trim a Binary Search Tree.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+//# Tutorial: https://www.youtube.com/watch?v=hFwakLj7wFA
+
+struct TreeNode
+{
+ int val;
+ TreeNode *left;
+ TreeNode *right;
+ TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+ TreeNode *trimBST(TreeNode *root, int low, int high)
+ {
+  if (root == NULL)
+  {
+   return NULL;
+  }
+
+  if (root->val > high)
+  {
+   return trimBST(root->left, low, high);
+  }
+
+  if (root->val < low)
+  {
+   return trimBST(root->right, low, high);
+  }
+
+  root->left = trimBST(root->left, low, high);
+  root->right = trimBST(root->right, low, high);
+
+  return root;
+ }
+};
