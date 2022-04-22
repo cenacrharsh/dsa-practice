9bab9e0e88480aab45f1eb4c93d6978900dd0ba9
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-04-22 10:34:36 +0530

    98. Validate Binary Search Tree

---
+++ Leetcode/Medium/98. Validate Binary Search Tree.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+struct TreeNode
+{
+ int val;
+ TreeNode *left;
+ TreeNode *right;
+ TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//! All Parent Nodes Lie In A Range
+
+class Solution
+{
+public:
+ static bool isValidBSTHelper(TreeNode *root, long min, long max)
+ {
+  if (root == NULL)
+  {
+   return true;
+  }
+
+  if (root->val <= min || root->val >= max)
+  {
+   return false;
+  }
+
+  return (isValidBSTHelper(root->left, min, root->val) && isValidBSTHelper(root->right, root->val, max));
+ }
+
+ bool isValidBST(TreeNode *root)
+ {
+  return isValidBSTHelper(root, LONG_MIN, LONG_MAX);
+ }
+};
+
+//! Inorder Traversal
+
+class Solution
+{
+ long prev = LONG_MIN;
+
+public:
+ bool isValidBST(TreeNode *root)
+ {
+  if (root == NULL)
+  {
+   return true;
+  }
+
+  if (!isValidBST(root->left))
+  {
+   return false;
+  }
+
+  if (root->val <= prev)
+  {
+   return false;
+  }
+
+  prev = root->val;
+
+  return isValidBST(root->right);
+ }
+};
