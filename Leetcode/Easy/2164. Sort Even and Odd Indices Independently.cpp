df01bb6d83803c2493ab67c33e195219b221c8e5
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-05-03 02:12:34 +0530

    922. Sort Array By Parity II

--- Leetcode/Easy/2164. Sort Even and Odd Indices Independently.cpp
+++ Leetcode/Easy/2164. Sort Even and Odd Indices Independently.cpp
@@ -6,6 +6,54 @@
 #include <cstring>
 using namespace std;
 
+//! 2 Pointers
+
+/*
+> We follow the below algorithm until one of the pointers reaches the other end of the array (here i represents the even pointer, and j represents the odd pointer):
+
+* Keep incrementing i by 2 until either it reaches the other end, or an outlier (odd number at even index).
+
+* Keep decrementing j by 2 until either it reaches the other end, or an outlier (even number at odd index).
+
+* Swap nums[i] and nums[j]
+*/
+
+/*
+> Time Complexity: O(n)
+> Space Complexity: O(1)
+*/
+
+class Solution
+{
+public:
+ vector<int> sortArrayByParityII(vector<int> &nums)
+ {
+  for (int even = 0, odd = 1; even < nums.size(); even += 2, odd += 2)
+  {
+   // while loop breaks off at first odd no. at even pos
+   while (even < nums.size() && nums[even] % 2 == 0)
+    even += 2;
+
+   // while loop breaks off at first even no. at odd pos
+   while (odd < nums.size() && nums[odd] % 2 == 1)
+    odd += 2;
+
+   if (even < nums.size())
+   {
+    swap(nums[even], nums[odd]);
+   }
+  }
+  return nums;
+ }
+};
+
+//! Extra Vectors
+
+/*
+> Time Complexity: O(n)
+> Space Complexity: O(n)
+*/
+
 class Solution
 {
 public:
