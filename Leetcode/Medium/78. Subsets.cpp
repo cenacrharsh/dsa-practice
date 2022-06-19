d98eb4fe7080b57b203825ccb78cf7d23362cbdf
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-06-19 23:22:22 +0530

    78. Subsets

--- Leetcode/Medium/78. Subsets.cpp
+++ Leetcode/Medium/78. Subsets.cpp
@@ -6,7 +6,32 @@
 #include <cstring>
 using namespace std;
 
-//! Using Recursion
+//! Using Bit Manipulation
+
+class Solution
+{
+public:
+ vector<vector<int>> subsets(vector<int> &nums)
+ {
+  int n = nums.size();
+  vector<vector<int>> ans;
+  for (int num = 0; num < (1 << n); num++)
+  {
+   vector<int> subset;
+   for (int i = 0; i < n; i++)
+   {
+    if ((num & (1 << i)) != 0)
+    {
+     subset.push_back(nums[i]);
+    }
+   }
+   ans.push_back(subset);
+  }
+  return ans;
+ }
+};
+
+//! Using Backtracking
 
 class Solution
 {
