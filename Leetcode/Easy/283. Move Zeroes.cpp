102d59f65581d2b38f49404bf7d822f9135bc7cf
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-07-03 12:56:00 +0530

    indentation fix 283

--- Leetcode/Easy/283. Move Zeroes.cpp
+++ Leetcode/Easy/283. Move Zeroes.cpp
@@ -1,32 +1,34 @@
-#include<bits/stdc++.h>
-#include<vector>
-#include<algorithm>
-#include<climits>
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
 #include <unordered_map>
 #include <cstring>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void moveZeroes(vector<int>& nums) {
-        int cnt = 0;
-        int k = 0;
-        
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(nums[i] != 0)
-            {
-                nums[k++] = nums[i];
-            }
-            else
-            {
-                cnt++;
-            }
-        }
-        
-        for(int i = 0; i < cnt; i++)
-        {
-            nums[k++] = 0;
-        }
-    }
+ void moveZeroes(vector<int> &nums)
+ {
+  int cnt = 0;
+  int k = 0;
+
+  for (int i = 0; i < nums.size(); i++)
+  {
+   if (nums[i] != 0)
+   {
+    nums[k++] = nums[i];
+   }
+   else
+   {
+    cnt++;
+   }
+  }
+
+  for (int i = 0; i < cnt; i++)
+  {
+   nums[k++] = 0;
+  }
+ }
 };
