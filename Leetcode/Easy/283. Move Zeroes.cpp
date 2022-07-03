1b3de7dec6fe0d3cb27811d1351e69e5502259f5
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-07-03 12:55:36 +0530

    283. Move Zeroes

---
+++ Leetcode/Easy/283. Move Zeroes.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<algorithm>
+#include<climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int cnt = 0;
+        int k = 0;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[k++] = nums[i];
+            }
+            else
+            {
+                cnt++;
+            }
+        }
+        
+        for(int i = 0; i < cnt; i++)
+        {
+            nums[k++] = 0;
+        }
+    }
+};
