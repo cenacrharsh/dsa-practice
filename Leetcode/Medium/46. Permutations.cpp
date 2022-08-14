093007e404b750eefb79bdfb33df2cee5087c082
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-08-14 21:47:31 +0530

    46. Permutations

--- Leetcode/Medium/46. Permutations.cpp
+++ Leetcode/Medium/46. Permutations.cpp
@@ -8,6 +8,36 @@ using namespace std;
 
 /*
 > Time Complexity: O(n! * n)
+> Space Complexity: O(n)
+*/
+
+class Solution
+{
+public:
+ void helper(vector<int> &nums, vector<vector<int>> &ans, int index)
+ {
+  if (index == nums.size())
+  {
+   ans.push_back(nums);
+   return;
+  }
+  for (int i = index; i < nums.size(); i++)
+  {
+   swap(nums[index], nums[i]);
+   helper(nums, ans, index + 1);
+   swap(nums[index], nums[i]);
+  }
+ }
+ vector<vector<int>> permute(vector<int> &nums)
+ {
+  vector<vector<int>> ans;
+  helper(nums, ans, 0);
+  return ans;
+ }
+};
+
+/*
+> Time Complexity: O(n! * n)
 > Space Complexity: O(n) + O(n)
 */
 
