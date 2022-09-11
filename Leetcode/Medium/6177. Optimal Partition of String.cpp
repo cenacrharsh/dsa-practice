a6c290e17a8b9cda9b7d292f107f87f7e91aa2f4
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-09-11 09:25:49 +0530

    6177. Optimal Partition of String

---
+++ Leetcode/Medium/6177. Optimal Partition of String.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+//! Contes Solution
+class Solution
+{
+public:
+ int partitionString(string s)
+ {
+  int count = 0;
+  for (int i = 0; i <= s.length() - 1; i++)
+  {
+   bool unique = true;
+   int freq[26] = {0};
+   while (unique && (i <= s.length() - 1))
+   {
+    if (freq[s[i] - 'a'] == 0)
+    {
+     freq[s[i] - 'a']++;
+     i++;
+    }
+    else
+    {
+     count++;
+     i--;
+     unique = false;
+    }
+   }
+  }
+  return ++count;
+ }
+};
