bae19f735d92857992b3f5ff67884ac16172be5b
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-12-03 23:43:48 +0530

    451. Sort Characters By Frequency

--- Leetcode/Medium/451. Sort Characters By Frequency.cpp
+++ Leetcode/Medium/451. Sort Characters By Frequency.cpp
@@ -6,6 +6,39 @@
 #include <cstring>
 using namespace std;
 
+//# Tutorial: https://www.youtube.com/watch?v=4Vu626AoElw&t=2s
+
+//! Using Hashmap & Vectors
+
+class Solution
+{
+public:
+ string frequencySort(string s)
+ {
+  unordered_map<char, int> freq;
+  vector<string> bucket(s.size() + 1, "");
+  string ans;
+  for (char c : s)
+  {
+   freq[c]++;
+  }
+  for (auto &it : freq)
+  {
+   int n = it.second;
+   char c = it.first;
+   bucket[n].append(n, c); // append char c n times at Nth position in vector
+  }
+  for (int i = s.size(); i >= 0; i--)
+  {
+   if (!bucket[i].empty())
+   {
+    ans.append(bucket[i]);
+   }
+  }
+  return ans;
+ }
+};
+
 //# Tutorial: https://www.youtube.com/watch?v=tbJt-0oazp4, https://www.youtube.com/watch?v=trFw8IDw2Vg,
 
 //! Using Hashmap & Priority Queues
