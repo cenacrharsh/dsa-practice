c620ecdcbb48de32e03091eec7fe8a672b0a495e
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-12-10 13:37:27 +0530

    509. Fibonacci Number

---
+++ Leetcode/Easy/509. Fibonacci Number.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Solution
+{
+public:
+ int fib(int n)
+ {
+  if (n == 0)
+   return 0;
+  if (n == 1)
+   return 1;
+  int fibo[n + 1];
+  fibo[0] = 0;
+  fibo[1] = 1;
+  for (int i = 2; i <= n; i++)
+  {
+   fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+  return fibo[n];
+ }
+};
