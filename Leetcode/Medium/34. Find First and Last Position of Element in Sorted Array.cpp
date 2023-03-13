05ed20591ad2225d5079c6cf71286fa626becaad
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-03-13 14:27:52 +0530

    34. Find First and Last Position of Element in Sorted Array

--- Leetcode/Medium/34. Find First and Last Position of Element in Sorted Array.cpp
+++ Leetcode/Medium/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -3,7 +3,74 @@
 #include <algorithm>
 #include <climits>
 using namespace std;
-
+class Solution
+{
+public:
+ int findFirstPosition(vector<int> &nums, int target)
+ {
+  int low = 0, high = nums.size() - 1;
+  while (low <= high)
+  {
+   int mid = low + (high - low) / 2;
+   if (target == nums[mid])
+   {
+    if (mid - 1 >= 0 && target == nums[mid - 1])
+    {
+     high = mid - 1;
+    }
+    else
+    {
+     return mid;
+    }
+   }
+   else if (target < nums[mid])
+   {
+    high = mid - 1;
+   }
+   else if (target > nums[mid])
+   {
+    low = mid + 1;
+   }
+  }
+  return -1;
+ }
+ int findLastPosition(vector<int> &nums, int target)
+ {
+  int low = 0, high = nums.size() - 1;
+  while (low <= high)
+  {
+   int mid = low + (high - low) / 2;
+   if (target == nums[mid])
+   {
+    if (mid + 1 < nums.size() && target == nums[mid + 1])
+    {
+     low = mid + 1;
+    }
+    else
+    {
+     return mid;
+    }
+   }
+   else if (target < nums[mid])
+   {
+    high = mid - 1;
+   }
+   else if (target > nums[mid])
+   {
+    low = mid + 1;
+   }
+  }
+  return -1;
+ }
+ vector<int> searchRange(vector<int> &nums, int target)
+ {
+  vector<int> ans(2, 0);
+  ans[0] = findFirstPosition(nums, target);
+  ans[1] = findLastPosition(nums, target);
+  return ans;
+ }
+};
+//* or
 class Solution
 {
 public:
