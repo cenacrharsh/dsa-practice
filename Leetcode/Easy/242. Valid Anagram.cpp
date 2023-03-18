4137c0cf33efd247fad81a97c4a57a221c07edc8
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-03-18 15:15:21 +0530

    242. Valid Anagram

--- Leetcode/Easy/242. Valid Anagram.cpp
+++ Leetcode/Easy/242. Valid Anagram.cpp
@@ -4,6 +4,33 @@
 #include <climits>
 using namespace std;
 
+//! 1 array
+class Solution
+{
+public:
+ bool isAnagram(string s, string t)
+ {
+  vector<int> freq(26, 0);
+  for (int i = 0; i < s.size(); i++)
+  {
+   freq[s[i] - 'a']++;
+  }
+  for (int i = 0; i < t.size(); i++)
+  {
+   freq[t[i] - 'a']--;
+  }
+  for (int i = 0; i < 26; i++)
+  {
+   if (freq[i] != 0)
+   {
+    return false;
+   }
+  }
+  return true;
+ }
+};
+
+//! 2 arrays
 class Solution
 {
 public:
