34685631b45c1b1f45c91901c84662eda016a478
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-03-23 20:43:25 +0530

    70. Climbing Stairs

--- Leetcode/Easy/70. Climbing Stairs.cpp
+++ Leetcode/Easy/70. Climbing Stairs.cpp
@@ -6,6 +6,24 @@
 #include <cstring>
 using namespace std;
 
+//! Bottom Up Approach + Tabulation
+class Solution
+{
+public:
+ int climbStairs(int n)
+ {
+  vector<int> stairs(n + 1, 0);
+  stairs[1] = 1; // one way to climb first stair
+  stairs[2] = 2; // 2 ways to climb 2nd stair, 1 step or 2 step
+  for (int i = 3; i <= n; i++)
+  {
+   stairs[i] = stairs[i - 1] + stairs[i - 2]; // since we can reach step n from step n-1 using 1 step and step n-2 using 2 steps, and we are calculating ways to actual steps taken, so reaching n-2th step in m ways then climbing to nth step in 2 steps also makes m ways to reach nth step
+  }
+  return stairs[n];
+ }
+};
+
+//! Using Top Down Approach
 class Solution
 {
 public:
