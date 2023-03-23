6b2aa46118e75c1fb01d2266c136768d193cb4f6
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-03-23 23:07:00 +0530

    94. Binary Tree Inorder Traversal

---
+++ Leetcode/Easy/94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+struct TreeNode
+{
+ int val;
+ TreeNode *left;
+ TreeNode *right;
+ TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//! Using Iteration
+
+class Solution
+{
+public:
+ vector<int> inorderTraversal(TreeNode *root)
+ {
+  vector<int> ans;
+  if (root == NULL)
+   return ans;
+
+  stack<TreeNode *> nodesLeft;
+  TreeNode *currNode = root;
+
+  while (currNode != NULL || !nodesLeft.empty())
+  {
+   // push all the left nodes of the currNode
+   while (currNode != NULL)
+   {
+    nodesLeft.push(currNode);
+    currNode = currNode->left;
+   }
+
+   // pop out the last pushed node from stack
+   currNode = nodesLeft.top();
+   nodesLeft.pop();
+   ans.push_back(currNode->val);
+
+   // now move to the right side of the currNode if there is any
+   currNode = currNode->right;
+  }
+  return ans;
+ }
+};
+
+//! Using Recursion
+
+class Solution
+{
+public:
+ void helper(TreeNode *root, vector<int> &ans)
+ {
+  if (root == NULL)
+   return;
+
+  // left
+  helper(root->left, ans);
+
+  // root
+  ans.push_back(root->val);
+
+  // right
+  helper(root->right, ans);
+ }
+ vector<int> inorderTraversal(TreeNode *root)
+ {
+  vector<int> ans;
+  helper(root, ans);
+  return ans;
+ }
+};
