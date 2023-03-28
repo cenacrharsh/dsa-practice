e0279c874992fab11161dcb5ebac25ba245ce0fa
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-03-28 11:10:10 +0530

    509. Fibonacci Number

--- Leetcode/Easy/509. Fibonacci Number.cpp
+++ Leetcode/Easy/509. Fibonacci Number.cpp
@@ -6,22 +6,122 @@
 #include <cstring>
 using namespace std;
 
+// # Tutorial: https://www.youtube.com/watch?v=tyB0ztf0DNY&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=3
+
+//! Space Optimized DP
+
+/*
+> Time Complexity: O(n)
+> Space Complexity: O(1)
+*/
+
 class Solution
 {
 public:
  int fib(int n)
  {
-  if (n == 0)
-   return 0;
-  if (n == 1)
-   return 1;
-  int fibo[n + 1];
-  fibo[0] = 0;
-  fibo[1] = 1;
+  if (n == 0 || n == 1)
+  {
+   return n;
+  }
+  int secondPrev = 0;
+  int prev = 1;
   for (int i = 2; i <= n; i++)
   {
-   fibo[i] = fibo[i - 1] + fibo[i - 2];
+   int curr = prev + secondPrev;
+   secondPrev = prev;
+   prev = curr;
   }
-  return fibo[n];
+  //* as when loop finishes, the last calculated value of curr in stored in prev
+  return prev;
+ }
+};
+
+//! DP
+
+/*
+> Time Complexity: O(n)
+> Space Complexity: O(n)
+*/
+
+class Solution
+{
+public:
+ int fib(int n)
+ {
+  // we need base case as if n == 0, we can't access dp[1]
+  if (n == 0 || n == 1)
+  {
+   return n;
+  }
+  vector<int> dp(n + 1, -1);
+  dp[0] = 0;
+  dp[1] = 1;
+  for (int i = 2; i <= n; i++)
+  {
+   //* intuition for space optimization, all we need to calculate dp[i] is the value of previous dp and second previous dp, not the entire array
+   dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+ }
+};
+
+//! Memoization
+
+/*
+> Time Complexity: O(n)
+> Space Complexity: O(n) + O(n) => dp vector + stack space
+*/
+
+class Solution
+{
+public:
+ int helper(int n, vector<int> &dp)
+ {
+  if (n == 0 || n == 1)
+  {
+   return n;
+  }
+
+  if (dp[n] != -1)
+  {
+   return dp[n];
+  }
+  else
+  {
+   return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+  }
+
+  return dp[n];
+ }
+ int fib(int n)
+ {
+  vector<int> dp(n + 1, -1);
+  return helper(n, dp);
+ }
+};
+
+//! Recursion
+
+/*
+> Time Complexity: O(2^n) => exponential
+> Space Complexity: O(2^n) => auxiliary stack space
+*/
+
+class Solution
+{
+public:
+ int helper(int n)
+ {
+  if (n == 0 || n == 1)
+  {
+   return n;
+  }
+
+  return helper(n - 1) + helper(n - 2);
+ }
+ int fib(int n)
+ {
+  return helper(n);
  }
 };
