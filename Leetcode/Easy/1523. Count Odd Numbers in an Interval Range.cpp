f35afffdf365c6a287828155e0db9b12da96fecd
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-03-30 20:43:20 +0530

    1523. Count Odd Numbers in an Interval Range

--- Leetcode/Easy/1523. Count Odd Numbers in an Interval Range.cpp
+++ Leetcode/Easy/1523. Count Odd Numbers in an Interval Range.cpp
@@ -14,6 +14,10 @@ x << 1 is equivalent to x * 2
 
 //! either both odd, or either one is odd in both cases h - l / 2 + 1 is the answer, so in case both even we increment and make it odd so we can use the same formula
 
+/*
+Let's start with an odd integer, say x; what would be the next odd integer? It would be x + 2; as explained above, there is exactly one even integer between every two odd integers, and hence all odd integers are equally spaced with a gap of one integer. By trying out some examples, we can deduce that the count of odd integers between x and a greater integer y where x is odd would be (y-x)/2 + 1
+*/
+
 class Solution
 {
 public:
@@ -28,5 +32,24 @@ public:
   // low could become greater than high due to incrementation
   // if it is, the answer is 0; otherwise, use the formula
   return low > high ? 0 : (((high - low) / 2) + 1);
+ }
+};
+
+//! Bit Manipulation
+class Solution
+{
+public:
+ int countOdds(int low, int high)
+ {
+  int count = 0;
+  for (int i = low; i <= high; i++)
+  {
+   // i & 1 = 1, tells last bit is 1 i.e. odd
+   if (i & 1 == 1)
+   {
+    count++;
+   }
+  }
+  return count;
  }
 };
