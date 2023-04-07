6581e1464e8bfebb337481ebeba5e0efa4cc7217
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-04-07 17:43:16 +0530

    389. Find the Difference

--- Leetcode/Easy/389. Find the Difference.cpp
+++ Leetcode/Easy/389. Find the Difference.cpp
@@ -11,15 +11,15 @@ using namespace std;
 class Solution
 {
 public:
- char findTheDifference(string s, string t)
- {
-  char c = 0;
-  for (char cs : s)
-   c ^= cs;
-  for (char ct : t)
-   c ^= ct;
-  return c;
- }
+    char findTheDifference(string s, string t)
+    {
+        char c = 0;
+        for (char cs : s)
+            c ^= cs;
+        for (char ct : t)
+            c ^= ct;
+        return c;
+    }
 };
 
 //! ASCII ( using ch - ch = 0 )
@@ -27,25 +27,25 @@ public:
 class Solution
 {
 public:
- char findTheDifference(string s, string t)
- {
-  char c = 0;
-  for (char ch : t)
-  {
-   c += ch;
-  }
-  for (char ch : s)
-  {
-   c -= ch;
-  }
-  return c;
-  // int ans = 0;
-  // for (auto &x : t)
-  //  ans += x; // int+char=>int+ASCII of char
-  // for (auto &x : s)
-  //  ans -= x;
-  // return char(ans); // converting ASCII to char
- }
+    char findTheDifference(string s, string t)
+    {
+        char c = 0;
+        for (char ch : t)
+        {
+            c += ch;
+        }
+        for (char ch : s)
+        {
+            c -= ch;
+        }
+        return c;
+        // int ans = 0;
+        // for (auto &x : t)
+        //  ans += x; // int+char=>int+ASCII of char
+        // for (auto &x : s)
+        //  ans -= x;
+        // return char(ans); // converting ASCII to char
+    }
 };
 
 //! Freq Array
@@ -53,22 +53,22 @@ public:
 class Solution
 {
 public:
- char findTheDifference(string s, string t)
- {
-  vector<int> freq(26);
-  char ans;
-  for (int i = 0; i < s.size(); i++)
-  {
-   freq[s[i] - 'a']++;
-  }
-  for (int i = 0; i < t.size(); i++)
-  {
-   if (freq[t[i] - 'a'] == 0)
-   {
-    ans = t[i];
-   }
-   freq[t[i] - 'a']--;
-  }
-  return ans;
- }
+    char findTheDifference(string s, string t)
+    {
+        vector<int> freq(26);
+        char ans;
+        for (int i = 0; i < s.size(); i++)
+        {
+            freq[s[i] - 'a']++;
+        }
+        for (int i = 0; i < t.size(); i++)
+        {
+            if (freq[t[i] - 'a'] == 0)
+            {
+                ans = t[i];
+            }
+            freq[t[i] - 'a']--;
+        }
+        return ans;
+    }
 };
