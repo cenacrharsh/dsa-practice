450e1e4cff670462cc5007bc2141ed01b323c361
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-04-20 23:45:44 +0530

    150. Evaluate Reverse Polish Notation

--- Leetcode/Medium/150. Evaluate Reverse Polish Notation.cpp
+++ Leetcode/Medium/150. Evaluate Reverse Polish Notation.cpp
@@ -9,45 +9,45 @@ using namespace std;
 class Solution
 {
 public:
- int evalRPN(vector<string> &tokens)
- {
-  stack<int> st;
-
-  for (int i = 0; i < tokens.size(); i++)
-  {
-   if (tokens[i] == "+" or tokens[i] == "-" or tokens[i] == "*" or tokens[i] == "/")
-   {
-    int op2 = st.top();
-    st.pop();
-    int op1 = st.top();
-    st.pop();
-
-    if (tokens[i] == "+")
+    int evalRPN(vector<string> &tokens)
     {
-     st.push(op1 + op2);
-    }
-    else if (tokens[i] == "-")
-    {
-     st.push(op1 - op2);
-    }
+        stack<int> st;
 
-    else if (tokens[i] == "*")
-    {
-     st.push(op1 * op2);
-    }
+        for (int i = 0; i < tokens.size(); i++)
+        {
+            if (tokens[i] == "+" or tokens[i] == "-" or tokens[i] == "*" or tokens[i] == "/")
+            {
+                int op2 = st.top();
+                st.pop();
+                int op1 = st.top();
+                st.pop();
 
-    else if (tokens[i] == "/")
-    {
-     st.push(op1 / op2);
-    }
-   }
-   else
-   {
-    int ele = stoi(tokens[i]);
-    st.push(ele);
-   }
-  }
+                if (tokens[i] == "+")
+                {
+                    st.push(op1 + op2);
+                }
+                else if (tokens[i] == "-")
+                {
+                    st.push(op1 - op2);
+                }
 
-  return st.top();
- }
+                else if (tokens[i] == "*")
+                {
+                    st.push(op1 * op2);
+                }
+
+                else if (tokens[i] == "/")
+                {
+                    st.push(op1 / op2);
+                }
+            }
+            else
+            {
+                int ele = stoi(tokens[i]);
+                st.push(ele);
+            }
+        }
+
+        return st.top();
+    }
 };
