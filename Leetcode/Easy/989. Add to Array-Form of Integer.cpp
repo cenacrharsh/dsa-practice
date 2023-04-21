73c656d9549e9e1295ce3692541aa02604cc18e7
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-04-21 12:19:00 +0530

    989. Add to Array-Form of Integer

--- Leetcode/Easy/989. Add to Array-Form of Integer.cpp
+++ Leetcode/Easy/989. Add to Array-Form of Integer.cpp
@@ -9,23 +9,23 @@ using namespace std;
 class Solution
 {
 public:
- vector<int> addToArrayForm(vector<int> &num, int k)
- {
-  int carry = 0;
-  int n = num.size();
-  int i = n - 1;
-  while (i >= 0)
-  {
-   int sum = i == n - 1 ? num[i] + carry + k : num[i] + carry;
-   num[i] = sum % 10;
-   carry = sum / 10;
-   i--;
-  }
-  while (carry)
-  {
-   num.insert(num.begin(), carry % 10);
-   carry /= 10;
-  }
-  return num;
- }
+    vector<int> addToArrayForm(vector<int> &num, int k)
+    {
+        int carry = 0;
+        int n = num.size();
+        int i = n - 1;
+        while (i >= 0)
+        {
+            int sum = i == n - 1 ? num[i] + carry + k : num[i] + carry;
+            num[i] = sum % 10;
+            carry = sum / 10;
+            i--;
+        }
+        while (carry)
+        {
+            num.insert(num.begin(), carry % 10);
+            carry /= 10;
+        }
+        return num;
+    }
 };
