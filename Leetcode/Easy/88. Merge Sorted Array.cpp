48bbde3b83a961d73f02edd810db0c193a80cbb4
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-05-25 20:55:43 +0530

    56. Merge Intervals

--- Leetcode/Easy/88. Merge Sorted Array.cpp
+++ Leetcode/Easy/88. Merge Sorted Array.cpp
@@ -6,11 +6,38 @@
 #include <cstring>
 using namespace std;
 
+// # Tutorial: https://www.youtube.com/watch?v=n7uwj04E0I4&list=PLgUwDviBIf0oF6QL8m22w1hIDC1vJ_BHz&index=39
+
 //! Without Extra Space
 
 //* compare the largest element in nums1 and smallest element in nums2, if largest element of nums1 is > smallest element of nums2 then we swap and move forward, once we find an element in nums1 and nums2 such that nums1 < nums2 we don't have to check any further since arr is sorted
 
+class Solution
+{
+public:
+    // Function to merge the arrays.
+    void merge(long long arr1[], long long arr2[], int n, int m)
+    {
+        int left = n - 1;
+        int right = 0;
+        while (left >= 0 && right < m)
+        {
+            if (arr1[left] > arr2[right])
+            {
+                swap(arr1[left], arr2[right]);
+                left--;
+                right++;
+            }
+            else
+            {
+                break;
+            }
+        }
 
+        sort(arr1, arr1 + n);
+        sort(arr2, arr2 + m);
+    }
+};
 
 //! With Extra Space
 class Solution
