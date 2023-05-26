1757397a0380167115ccf40052800e328e3cfa79
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-05-27 00:18:24 +0530

    50. Pow(x, n)

--- Leetcode/Medium/50. Pow(x, n).cpp
+++ Leetcode/Medium/50. Pow(x, n).cpp
@@ -13,27 +13,63 @@ using namespace std;
 > Space Complexity: O(1)
 */
 
-class Solution {
+//! Recursive
+class Solution
+{
 public:
-    double myPow(double x, int n) {
+    double myPow(double x, int n)
+    {
+        if (n == 0)
+            return 1.0;
+
+        // if power is negative
+        if (n < 0)
+        {
+            x = 1 / x;
+            n = abs(n);
+        }
+
+        // even power
+        if (n % 2 == 0)
+        {
+            return myPow(x * x, n / 2);
+        }
+
+        // odd power
+        return x * myPow(x, n - 1);
+    }
+};
+
+//! Iterative
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
         double ans = 1.0;
         long long power = n;
 
-        if(power < 0) {
+        if (power < 0)
+        {
             power = -1 * power;
         }
 
-        while(power != 0) {
-            if(power % 2 == 0) {
+        while (power != 0)
+        {
+            if (power % 2 == 0)
+            {
                 x = x * x;
                 power = power / 2;
-            } else {
+            }
+            else
+            {
                 ans = ans * x;
                 power = power - 1;
             }
         }
 
-        if(n < 0) {
+        if (n < 0)
+        {
             ans = (double)(1.0) / (double)(ans);
         }
         return ans;
