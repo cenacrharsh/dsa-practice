cff2b57adc2299a2ca8abc9c9c3f1bba18a0b322
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-06-06 14:31:33 +0530

    1838. Frequency of the Most Frequent Element

---
+++ Leetcode/Medium/1838. Frequency of the Most Frequent Element.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+/*
+> Time Complexity: O(n * log(n))
+> Space Complexity: O(1)
+*/
+
+//! Sliding Window
+class Solution
+{
+public:
+    int maxFrequency(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+
+        int l = 0, r = 0, maxLen = 0;
+        long long total = 0;
+
+        while (r < nums.size())
+        {
+            total += nums[r];
+
+            while ((long long)nums[r] * (r - l + 1) - total > k)
+            {
+                total -= nums[l];
+                l++;
+            }
+
+            maxLen = max(maxLen, r - l + 1);
+            r++;
+        }
+
+        return maxLen;
+    }
+};
