ff78730a617b6a7dfd3a85453cd7db4655734f60
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-08 12:02:33 +0530

    74. Search a 2D Matrix

--- Leetcode/Medium/74. Search a 2D Matrix.cpp
+++ Leetcode/Medium/74. Search a 2D Matrix.cpp
@@ -13,33 +13,34 @@ using namespace std;
 class Solution
 {
 public:
- bool searchMatrix(vector<vector<int>> &matrix, int target)
- {
-  // treating the 2D array as one big 1D array
-  int N = matrix.size();
-  int M = matrix[0].size();
-  int low = 0;
-  int high = M * N - 1;
-  while (low <= high)
-  {
-   int mid = (low + (high - low) / 2);
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        //* treating the m * n 2D array as one big 1D array
+        //* m -> row and n -> col
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int low = 0;
+        int high = row * col - 1;
+        while (low <= high)
+        {
+            int mid = (low + (high - low) / 2);
 
-   if (matrix[mid / M][mid % M] == target)
-   {
-    return true;
-   }
+            if (target == matrix[mid / col][mid % col])
+            {
+                return true;
+            }
 
-   if (target > matrix[mid / M][mid % M])
-   {
-    low = mid + 1;
-   }
-   else
-   {
-    high = mid - 1;
-   }
-  }
-  return false;
- }
+            if (target > matrix[mid / col][mid % col])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
 };
 
 /*
@@ -52,31 +53,31 @@ public:
 class Solution
 {
 public:
- bool searchMatrix(vector<vector<int>> &matrix, int target)
- {
-  int M = matrix.size();
-  int N = matrix[0].size();
-  int row = 0, col = N - 1;
-  // since we move left so j >= 0
-  // since we move down so i < n
-  while (row < M && col >= 0)
-  {
-   if (matrix[row][col] == target)
-   {
-    return 1;
-   }
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int M = matrix.size();
+        int N = matrix[0].size();
+        int row = 0, col = N - 1;
+        // since we move left so j >= 0
+        // since we move down so i < n
+        while (row < M && col >= 0)
+        {
+            if (matrix[row][col] == target)
+            {
+                return 1;
+            }
 
-   if (target > matrix[row][col])
-   {
-    // if element if > the last element of that row, we move to next row
-    row++;
-   }
-   else
-   {
-    // if element if < the last element of that row, we move to lower col values to find the target
-    col--;
-   }
-  }
-  return 0;
- }
+            if (target > matrix[row][col])
+            {
+                // if element if > the last element of that row, we move to next row
+                row++;
+            }
+            else
+            {
+                // if element if < the last element of that row, we move to lower col values to find the target
+                col--;
+            }
+        }
+        return 0;
+    }
 };
