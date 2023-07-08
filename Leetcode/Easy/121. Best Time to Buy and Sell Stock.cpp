794e11f37f3a54a8de23fe79372196db339d6aa6
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-08 12:10:40 +0530

    121. Best Time to Buy and Sell Stock

--- Leetcode/Easy/121. Best Time to Buy and Sell Stock.cpp
+++ Leetcode/Easy/121. Best Time to Buy and Sell Stock.cpp
@@ -1,4 +1,4 @@
-//# Tutorial: https://www.youtube.com/watch?v=4YjEHmw1MX0&list=PL-Jc9J83PIiG8fE6rj9F5a6uyQ5WPdqKy&index=30
+// # Tutorial: https://www.youtube.com/watch?v=4YjEHmw1MX0&list=PL-Jc9J83PIiG8fE6rj9F5a6uyQ5WPdqKy&index=30
 
 //! One Transaction Allowed
 
@@ -8,19 +8,40 @@
 #include <climits>
 using namespace std;
 
+//! Left -> Right
 class Solution
 {
 public:
- int maxProfit(vector<int> &prices)
- {
-  int minPrice = prices[0];
-  int maxProfit = 0;
-  for (int i = 1; i < prices.size(); i++)
-  {
-   int tempProfit = prices[i] - minPrice;
-   maxProfit = max(maxProfit, tempProfit);
-   minPrice = min(minPrice, prices[i]);
-  }
-  return maxProfit;
- }
+    int maxProfit(vector<int> &prices)
+    {
+        int minSellingPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.size(); i++)
+        {
+            minSellingPrice = min(minSellingPrice, prices[i]);
+            int tempProfit = prices[i] - minSellingPrice;
+            maxProfit = max(maxProfit, tempProfit);
+        }
+        return maxProfit;
+    }
+};
+
+//! Right -> Left
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        int maxSellingPrice = INT_MIN;
+        int profit = 0;
+        int maxProfit = INT_MIN;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            maxSellingPrice = max(maxSellingPrice, prices[i]);
+            profit = maxSellingPrice - prices[i];
+            maxProfit = max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
 };
