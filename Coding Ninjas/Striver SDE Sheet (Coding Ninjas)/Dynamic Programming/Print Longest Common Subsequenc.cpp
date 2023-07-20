1ef65aaf25fabeef715c2e64467b39f0954b4d7b
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-20 14:05:26 +0530

    Print Longest Common Subsequence

--- Coding Ninjas/Striver SDE Sheet (Coding Ninjas)/Dynamic Programming/Print Longest Common Subsequenc.cpp
+++ Coding Ninjas/Striver SDE Sheet (Coding Ninjas)/Dynamic Programming/Print Longest Common Subsequenc.cpp
@@ -13,3 +13,55 @@ using namespace std;
 > Space Complexity: O(N + M)
 */
 
+string findLCS(int n, int m, string &s1, string &s2)
+{
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    // base case - fill zeroes in first rol and first col, already done in vectors
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    int ansLen = dp[n][m];
+    string ans = "";
+    for (int i = 1; i <= ansLen; i++)
+    {
+        ans += '$';
+    }
+
+    int i = n, j = m, k = ansLen - 1;
+    while (i > 0 && j > 0)
+    {
+        if (s1[i - 1] == s2[j - 1])
+        {
+            ans[k] = s1[i - 1];
+            k--;
+            i--;
+            j--;
+        }
+        else if (dp[i - 1][j] >= dp[i][j - 1])
+        {
+            // go up
+            i--;
+        }
+        else
+        {
+            // go left
+            j--;
+        }
+    }
+
+    return ans;
+}
