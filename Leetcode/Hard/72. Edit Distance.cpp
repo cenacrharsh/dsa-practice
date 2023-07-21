74ae485f186039a9c83c75fe19e68b820df9b17e
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-21 17:07:43 +0530

    72. Edit Distance

--- Leetcode/Hard/72. Edit Distance.cpp
+++ Leetcode/Hard/72. Edit Distance.cpp
@@ -6,106 +6,111 @@
 #include <cstring>
 using namespace std;
 
-// # Tutorial: https://www.youtube.com/watch?v=fJaKO8FbDdo&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=35
+// # Tutorial: https://www.youtube.com/watch?v=fJaKO8FbDdo&list=PLgUwDviBIf0pwFf-BnpkXxs0Ra0eU2sJY&index=18
 
-//! Brute Force Recursion
+//- f(n - 1, m - 1) => find minimum number of operations required to convert s1[0...n-1] to s2[0...m-1]
 
-/*
-> Time Complexity: O(exponential)
-> Space Complexity: O(n+m) => auxiliary stack space
-*/
+//! Space Optimized Tabulation DP
 
 class Solution
 {
 public:
-    int f(int i, int j, string &word1, string &word2)
-    {
-        if (i < 0)
-        {
-            return j + 1;
-        }
-        if (j < 0)
-        {
-            return i + 1;
-        }
-
-        if (word1[i] == word2[j])
-        {
-            return f(i - 1, j - 1, word1, word2);
-        }
-
-        // f(i, j-1) => insert
-        // f(i-1, j) => delete
-        // f(i-1, j-1) => replace
-        return 1 + min(f(i, j - 1, word1, word2), min(f(i - 1, j, word1, word2), f(i - 1, j - 1, word1, word2)));
-    }
     int minDistance(string word1, string word2)
     {
         int n = word1.size();
         int m = word2.size();
-        return f(n - 1, m - 1, word1, word2);
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
+
+        // base case 1, every col of first row has value = col number
+        for (int j = 0; j <= m; j++)
+        {
+            prev[j] = j;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            // base case 2, first col of every row has value = row number
+            curr[0] = i;
+
+            for (int j = 1; j <= m; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                {
+                    curr[j] = prev[j - 1];
+                }
+                else
+                {
+                    //* insert
+                    int insertOp = curr[j - 1];
+
+                    //* delete
+                    int deleteOp = prev[j];
+
+                    //* replace
+                    int replaceOp = prev[j - 1];
+
+                    curr[j] = 1 + min(insertOp, min(deleteOp, replaceOp));
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+};
+
+//! Tabulation DP -> Bottom Up Approach
+
+class Solution
+{
+public:
+    int minDistance(string word1, string word2)
+    {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // base case
+        for (int j = 0; j <= m; j++)
+        {
+            dp[0][j] = j;
+        }
+        for (int i = 0; i <= n; i++)
+        {
+            dp[i][0] = i;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    //* insert
+                    int insertOp = dp[i][j - 1];
+
+                    //* delete
+                    int deleteOp = dp[i - 1][j];
+
+                    //* replace
+                    int replaceOp = dp[i - 1][j - 1];
+
+                    dp[i][j] = 1 + min(insertOp, min(deleteOp, replaceOp));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
 
 //! Memoization DP
-
-/*
-> Time Complexity: O(n*m)
-> Space Complexity: O(n*m) + O(n+m)
-*/
-
 class Solution
 {
 public:
-    int f(int i, int j, string &word1, string &word2, vector<vector<int>> &dp)
-    {
-        if (i < 0)
-        {
-            return j + 1;
-        }
-        if (j < 0)
-        {
-            return i + 1;
-        }
-
-        if (dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-
-        if (word1[i] == word2[j])
-        {
-            return dp[i][j] = f(i - 1, j - 1, word1, word2, dp);
-        }
-
-        // f(i, j-1) => insert
-        // f(i-1, j) => delete
-        // f(i-1, j-1) => replace
-        return dp[i][j] = 1 + min(f(i, j - 1, word1, word2, dp),
-                                  min(f(i - 1, j, word1, word2, dp),
-                                      f(i - 1, j - 1, word1, word2, dp)));
-    }
-    int minDistance(string word1, string word2)
-    {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, word1, word2, dp);
-    }
-};
-
-//! Tabulation DP || Bottom Up
-
-/*
-> Time Complexity: O(n*m)
-> Space Complexity: O(n*m)
-*/
-
-//* first shift indexes
-class Solution
-{
-public:
-    int f(int i, int j, string &word1, string &word2, vector<vector<int>> &dp)
+    int helper(int i, int j, string &word1, string &word2, vector<vector<int>> &dp)
     {
         if (i == 0)
         {
@@ -123,107 +128,114 @@ public:
 
         if (word1[i - 1] == word2[j - 1])
         {
-            return dp[i][j] = f(i - 1, j - 1, word1, word2, dp);
+            return dp[i][j] = helper(i - 1, j - 1, word1, word2, dp);
         }
+        else
+        {
+            //* insert
+            int insertOp = helper(i, j - 1, word1, word2, dp);
 
-        // f(i, j-1) => insert
-        // f(i-1, j) => delete
-        // f(i-1, j-1) => replace
-        return dp[i][j] = 1 + min(f(i, j - 1, word1, word2, dp),
-                                  min(f(i - 1, j, word1, word2, dp),
-                                      f(i - 1, j - 1, word1, word2, dp)));
+            //* delete
+            int deleteOp = helper(i - 1, j, word1, word2, dp);
+
+            //* replace
+            int replaceOp = helper(i - 1, j - 1, word1, word2, dp);
+
+            return dp[i][j] = 1 + min(insertOp, min(deleteOp, replaceOp));
+        }
     }
+
     int minDistance(string word1, string word2)
     {
         int n = word1.size();
         int m = word2.size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return f(n, m, word1, word2, dp);
+        return helper(n, m, word1, word2, dp);
     }
 };
 
-//* now apply tabulation
+//! Recursion -> Top Down Approach
+
+//@ with index shifting
 class Solution
 {
 public:
+    int helper(int i, int j, string &word1, string &word2)
+    {
+        if (i == 0)
+        {
+            return j;
+        }
+        if (j == 0)
+        {
+            return i;
+        }
+
+        if (word1[i - 1] == word2[j - 1])
+        {
+            return helper(i - 1, j - 1, word1, word2);
+        }
+        else
+        {
+            //* insert
+            int insertOp = helper(i, j - 1, word1, word2);
+
+            //* delete
+            int deleteOp = helper(i - 1, j, word1, word2);
+
+            //* replace
+            int replaceOp = helper(i - 1, j - 1, word1, word2);
+
+            return 1 + min(insertOp, min(deleteOp, replaceOp));
+        }
+    }
+
     int minDistance(string word1, string word2)
     {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        // Base Cases
-        for (int i = 0; i <= n; i++)
-        {
-            dp[i][0] = i;
-        }
-        for (int j = 0; j <= m; j++)
-        {
-            dp[0][j] = j;
-        }
-
-        // Invert i and j
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                // same recurrence relation as Top Down
-                if (word1[i - 1] == word2[j - 1])
-                {
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-                else
-                {
-                    // f(i, j-1) => insert
-                    // f(i-1, j) => delete
-                    // f(i-1, j-1) => replace
-                    dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
-                }
-            }
-        }
-
-        return dp[n][m];
+        return helper(n, m, word1, word2);
     }
 };
 
-//* further space optimization using 1D array
+//@ without index shifting
 class Solution
 {
 public:
+    int helper(int i, int j, string &word1, string &word2)
+    {
+        if (i < 0)
+        {
+            return j + 1;
+        }
+        if (j < 0)
+        {
+            return i + 1;
+        }
+
+        if (word1[i] == word2[j])
+        {
+            return helper(i - 1, j - 1, word1, word2);
+        }
+        else
+        {
+            //* insert
+            int insertOp = helper(i, j - 1, word1, word2);
+
+            //* delete
+            int deleteOp = helper(i - 1, j, word1, word2);
+
+            //* replace
+            int replaceOp = helper(i - 1, j - 1, word1, word2);
+
+            return 1 + min(insertOp, min(deleteOp, replaceOp));
+        }
+    }
+
     int minDistance(string word1, string word2)
     {
         int n = word1.size();
         int m = word2.size();
-        vector<int> prev(m + 1, 0), curr(m + 1, 0);
-
-        // Base Cases
-        for (int j = 0; j <= m; j++)
-        {
-            prev[j] = j;
-        }
-
-        // Invert i and j
-        for (int i = 1; i <= n; i++)
-        {
-            curr[0] = i; // aq to 2nd base case, each rows first element is always == i
-            for (int j = 1; j <= m; j++)
-            {
-                // same recurrence relation as Top Down
-                if (word1[i - 1] == word2[j - 1])
-                {
-                    curr[j] = prev[j - 1];
-                }
-                else
-                {
-                    // f(i, j-1) => insert
-                    // f(i-1, j) => delete
-                    // f(i-1, j-1) => replace
-                    curr[j] = 1 + min(curr[j - 1], min(prev[j], prev[j - 1]));
-                }
-            }
-            prev = curr;
-        }
-
-        return prev[m];
+        return helper(n - 1, m - 1, word1, word2);
     }
 };
