731a2e9de2fde7df6bde842fd1ba8fde866839fb
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-26 11:06:50 +0530

    Longest Increasing Subsequence

---
+++ Coding Ninjas/Striver SDE Sheet (Coding Ninjas)/Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+int longestIncreasingSubsequence(int arr[], int n)
+{
+    vector<int> dp(n + 1, 1);
+
+    int maxLen = 1;
+    for (int curr = 0; curr < n; curr++)
+    {
+        for (int prev = 0; prev < curr; prev++)
+        {
+            if (arr[prev] < arr[curr])
+            {
+                dp[curr] = max(dp[curr], 1 + dp[prev]);
+            }
+        }
+        maxLen = max(maxLen, dp[curr]);
+    }
+    return maxLen;
+}
