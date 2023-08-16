623bfc8055926bf32d7915dd269a5000c753f658
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-08-16 10:27:54 +0530

    Detect Cycle in an Undirected Graph

--- Coding Ninjas/Striver SDE Sheet (Coding Ninjas)/Graphs/Detect Cycle in an Undirected Graph.cpp
+++ Coding Ninjas/Striver SDE Sheet (Coding Ninjas)/Graphs/Detect Cycle in an Undirected Graph.cpp
@@ -8,13 +8,63 @@ using namespace std;
 
 // # Tutorial: https://www.youtube.com/watch?v=BPlrALf1LDU&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=12
 
+//@ Summation of adj Nodes = Summation of degrees = 2E
+
+//! DFS
+
+/*
+> Time Complexity: O(N + 2E) + O(N) -> (traverse all nodes + for loop for visited array)
+> Space Complexity: O(N) + O(N) -> (visited array + recursive stack space)
+*/
+
+class Graph
+{
+private:
+    bool detect(int source, int parent, vector<int> adj[], int visited[])
+    {
+        visited[source] = 1;
+
+        for (int adjacentNode : adj[source])
+        {
+            if (visited[adjacentNode] == 0)
+            {
+                if (detect(adjacentNode, source, adj, visited) == true)
+                {
+                    return true;
+                }
+            }
+            else if (adjacentNode != parent)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+public:
+    bool detectCycle(int V, vector<int> adj[])
+    {
+        int visited[V] = {0};
+        for (int i = 0; i < V; i++)
+        {
+            if (visited[i] != 1)
+            {
+                if (detect(i, -1, adj, visited) == true)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//! BFS
+
 /*
 > Time Complexity: O(N + 2E) + O(N)
 > Space Complexity: O(N) + O(N)
 */
-
-//@ Summation of adj Nodes = Summation of degrees = 2E
-
 class Graph
 {
 private:
