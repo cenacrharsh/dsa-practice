4ee83dc3f927ed8d0d9392922cf652e50c2e8126
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-08-21 22:28:57 +0530

    237. Delete Node in a Linked List

---
+++ Leetcode/Medium/237. Delete Node in a Linked List.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        if (node != NULL && node->next != NULL)
+        {
+            ListNode *nextNode = node->next;
+            node->val = nextNode->val;
+            node->next = nextNode->next;
+        }
+    }
+};
