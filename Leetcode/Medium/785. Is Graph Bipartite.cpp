370ed9e46f34b83cc21fc6c419a288b6e643105a
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-08-23 12:42:07 +0530

    785. Is Graph Bipartite?

--- Leetcode/Medium/785. Is Graph Bipartite.cpp
+++ Leetcode/Medium/785. Is Graph Bipartite.cpp
@@ -8,6 +8,53 @@ using namespace std;
 
 // # Tutorial: https://www.youtube.com/watch?v=-vu34sct1g8&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=11
 
+//! DFS
+
+class Solution
+{
+public:
+    bool checkDfs(int node, int col, vector<vector<int>> &graph, vector<int> &color)
+    {
+        color[node] = col;
+
+        for (auto it : graph[node])
+        {
+            if (color[it] == -1)
+            {
+                if (checkDfs(it, !col, graph, color) == false)
+                {
+                    return false;
+                }
+            }
+            else if (color[it] == col)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>> &graph)
+    {
+        int V = graph.size();
+        vector<int> color(V, -1);
+
+        for (int i = 0; i < V; i++)
+        {
+            if (color[i] == -1)
+            {
+                if (checkDfs(i, 0, graph, color) == false)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+//! BFS
+
 class Solution
 {
 public:
