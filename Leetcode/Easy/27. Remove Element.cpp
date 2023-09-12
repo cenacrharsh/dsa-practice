93385e3725d585cd69371df00f27631459650bb5
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-09-13 01:07:38 +0530

    27. Remove Element

--- Leetcode/Easy/27. Remove Element.cpp
+++ Leetcode/Easy/27. Remove Element.cpp
@@ -9,16 +9,16 @@ using namespace std;
 class Solution
 {
 public:
- int removeElement(vector<int> &nums, int val)
- {
-  int k = 0;
-  for (int i = 0; i < nums.size(); i++)
-  {
-   if (nums[i] != val)
-   {
-    nums[k++] = nums[i];
-   }
-  }
-  return k;
- }
+    int removeElement(vector<int> &nums, int val)
+    {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
 };
