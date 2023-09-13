dfe5603950c68718b51c870065dee534e9fe2325
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-09-13 13:01:38 +0530

    80. Remove Duplicates from Sorted Array II

--- Leetcode/Medium/80. Remove Duplicates from Sorted Array II.cpp
+++ Leetcode/Medium/80. Remove Duplicates from Sorted Array II.cpp
@@ -9,32 +9,33 @@ using namespace std;
 class Solution
 {
 public:
- int removeDuplicates(vector<int> &nums)
- {
-  if (nums.size() < 2)
-  {
-   return nums.size();
-  }
-
-  int k = 2;
-  for (int i = 2; i < nums.size(); i++)
-  {
-   if (nums[i] == nums[k - 1])
-   {
-    if (nums[i] == nums[k - 2])
+    int removeDuplicates(vector<int> &nums)
     {
-     continue;
+        if (nums.size() < 2)
+        {
+            return nums.size();
+        }
+        int k = 2;
+        for (int i = 2; i < nums.size(); i++)
+        {
+            //* check with the last element inserted
+            if (nums[i] == nums[k - 1])
+            {
+                //* check with the second last element inserted
+                if (nums[i] == nums[k - 2])
+                {
+                    continue;
+                }
+                else
+                {
+                    nums[k++] = nums[i];
+                }
+            }
+            else
+            {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
     }
-    else
-    {
-     nums[k++] = nums[i];
-    }
-   }
-   else
-   {
-    nums[k++] = nums[i];
-   }
-  }
-  return k;
- }
 };
