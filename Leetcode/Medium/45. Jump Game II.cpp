91bde580460e9ba704743a71cc9de8ea82d4d2ee
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-09-16 10:27:18 +0530

    45. Jump Game II

--- Leetcode/Medium/45. Jump Game II.cpp
+++ Leetcode/Medium/45. Jump Game II.cpp
@@ -14,39 +14,39 @@ using namespace std;
 class Solution
 {
 public:
- int jump(vector<int> &nums)
- {
-  int n = nums.size();
-  int minJumpsReq[n];
-  minJumpsReq[n - 1] = 0;
-  for (int i = n - 2; i >= 0; i--)
-  {
-   int leastOfAllPossibleChoices = INT_MAX;
-   if (nums[i] != 0)
-   {
-    for (int j = 1; j <= nums[i]; j++)
+    int jump(vector<int> &nums)
     {
-     if (i + j < n)
-     {
-      leastOfAllPossibleChoices =
-          min(leastOfAllPossibleChoices, minJumpsReq[i + j]);
-     }
+        int n = nums.size();
+        int minJumpsReq[n];
+        minJumpsReq[n - 1] = 0;
+        for (int i = n - 2; i >= 0; i--)
+        {
+            int leastOfAllPossibleChoices = INT_MAX;
+            if (nums[i] != 0)
+            {
+                for (int j = 1; j <= nums[i]; j++)
+                {
+                    if (i + j < n)
+                    {
+                        leastOfAllPossibleChoices =
+                            min(leastOfAllPossibleChoices, minJumpsReq[i + j]);
+                    }
+                }
+            }
+            if (leastOfAllPossibleChoices == INT_MAX)
+            {
+                minJumpsReq[i] = INT_MAX;
+            }
+            else
+            {
+                minJumpsReq[i] = leastOfAllPossibleChoices + 1;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << minJumpsReq[i] << " ";
+        }
+        cout << endl;
+        return minJumpsReq[0];
     }
-   }
-   if (leastOfAllPossibleChoices == INT_MAX)
-   {
-    minJumpsReq[i] = INT_MAX;
-   }
-   else
-   {
-    minJumpsReq[i] = leastOfAllPossibleChoices + 1;
-   }
-  }
-  for (int i = 0; i < n; i++)
-  {
-   cout << minJumpsReq[i] << " ";
-  }
-  cout << endl;
-  return minJumpsReq[0];
- }
 };
