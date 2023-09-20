42d71a887535d509a59aeeb8e4d1ef693a7d1977
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-09-20 19:38:39 +0530

    704. Binary Search

--- Leetcode/Easy/704. Binary Search.cpp
+++ Leetcode/Easy/704. Binary Search.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 //! Upper Bound and Lower Bound
+
 //> index of element either = or just greater than target
 /*
 Vector contains : 10 20 30 30 30 40 50
@@ -16,31 +17,31 @@ lower_bound for element 55 at position : 7
 */
 int lower_bound(vector<int> &v, int target)
 {
-  int low = 0, high = v.size() - 1;
-  while (high - low > 1)
-  {
-    int mid = low + (high - low) / 2;
-    // we want to eliminate all lesser elements than target
-    if (v[mid] < target)
+    int low = 0, high = v.size() - 1;
+    while (high - low > 1)
     {
-      low = mid + 1;
+        int mid = low + (high - low) / 2;
+        // we want to eliminate all lesser elements than target
+        if (v[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            //* v[mid] >= target
+            high = mid;
+        }
     }
-    else
+    //> since lower bound means element which is either = or just greater than target, we have to compare with low first as if both low and high are eligible then low is our ans as it comes first
+    if (v[low] >= target)
     {
-      //* v[mid] >= target
-      high = mid;
+        return low;
     }
-  }
-  //> since lower bound means element which is either = or just greater than target, we have to compare with low first as if both low and high are eligible then low is our ans as it comes first
-  if (v[low] >= target)
-  {
-    return low;
-  }
-  if (v[high] >= target)
-  {
-    return high;
-  }
-  return -1;
+    if (v[high] >= target)
+    {
+        return high;
+    }
+    return -1;
 }
 
 //> first element that is bigger than target
@@ -52,31 +53,31 @@ upper_bound for element 45 is at position : 4
 */
 int upper_bound(vector<int> &v, int target)
 {
-  int low = 0, high = v.size() - 1;
-  while (high - low > 1)
-  {
-    int mid = low + (high - low) / 2;
-    // we have to eleminate elements <= target also
-    if (v[mid] <= target)
+    int low = 0, high = v.size() - 1;
+    while (high - low > 1)
     {
-      low = mid + 1;
+        int mid = low + (high - low) / 2;
+        // we have to eleminate elements <= target also
+        if (v[mid] <= target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            //* v[mid] > target
+            high = mid;
+        }
     }
-    else
+    //> since lower bound means element which is either = or just greater than target, we have to compare with low first as if both low and high are eligible then low is our ans as it comes first
+    if (v[low] > target)
     {
-      //* v[mid] > target
-      high = mid;
+        return low;
     }
-  }
-  //> since lower bound means element which is either = or just greater than target, we have to compare with low first as if both low and high are eligible then low is our ans as it comes first
-  if (v[low] > target)
-  {
-    return low;
-  }
-  if (v[high] > target)
-  {
-    return high;
-  }
-  return -1;
+    if (v[high] > target)
+    {
+        return high;
+    }
+    return -1;
 }
 
 /*
@@ -84,60 +85,67 @@ int upper_bound(vector<int> &v, int target)
 > Space Complexity: O(1)
 */
 
-//! Approach 1
+/***************************************************/
+
+/*
+> Time Complexity: O(log n)
+> Space Complexity: O()
+*/
+
+//! Iterative
 class Solution
 {
 public:
-  int search(vector<int> &nums, int target)
-  {
-    int l = 0;
-    int r = nums.size() - 1;
-    while (l <= r)
+    int search(vector<int> &nums, int target)
     {
-      int mid = l + (r - l) / 2;
-      if (nums[mid] == target)
-      {
-        return mid;
-      }
-      else if (target < nums[mid])
-      {
-        r = mid - 1;
-      }
-      else if (target > nums[mid])
-      {
-        l = mid + 1;
-      }
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (target < nums[mid])
+            {
+                r = mid - 1;
+            }
+            else if (target > nums[mid])
+            {
+                l = mid + 1;
+            }
+        }
+        return -1;
     }
-    return -1;
-  }
 };
 
-//! Approach 2
+//! Recursive
 class Solution
 {
 public:
-  int search(vector<int> &nums, int target)
-  {
-    int low = 0, high = nums.size() - 1;
-    // terminate loop when only 2 elements left in search space
-    while (high - low > 1)
+    int binarySearch(vector<int> &nums, int low, int high, int target)
     {
-      int mid = low + (high - low) / 2;
-      if (nums[mid] < target)
-      {
-        low = mid + 1;
-      }
-      else
-      {
-        // nums[mid] >= target
-        high = mid;
-      }
+        if (low > high)
+        {
+            return -1;
+        }
+
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (target > nums[mid])
+        {
+            return binarySearch(nums, mid + 1, high, target);
+        }
+        return binarySearch(nums, low, mid - 1, target);
     }
-    if (nums[low] == target)
-      return low;
-    else if (nums[high] == target)
-      return high;
-    else
-      return -1;
-  }
+
+    int search(vector<int> &nums, int target)
+    {
+        return binarySearch(nums, 0, nums.size() - 1, target);
+    }
 };
