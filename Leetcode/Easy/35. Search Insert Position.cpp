8590b403790fb0230a2b490083e445aa5f98b4fd
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-09-30 11:55:21 +0530

    35. Search Insert Position

--- Leetcode/Easy/35. Search Insert Position.cpp
+++ Leetcode/Easy/35. Search Insert Position.cpp
@@ -4,84 +4,27 @@
 #include <climits>
 using namespace std;
 
-//! Lower Bound Implemented
 class Solution
 {
 public:
- int searchInsert(vector<int> &nums, int target)
- {
-  int n = nums.size();
-  int low = 0, high = n - 1;
-  if (target > nums[n - 1])
-  {
-   return n;
-  }
-  while (high - low > 1)
-  {
-   int mid = low + (high - low) / 2;
-   if (nums[mid] < target)
-   {
-    low = mid + 1;
-   }
-   else
-   {
-    // nums[mid] >= target
-    high = mid;
-   }
-  }
-  if (nums[low] >= target)
-  {
-   return low;
-  }
-  else if (nums[high] >= target)
-  {
-   return high;
-  }
-  return -1;
- }
-};
-
-//* or
-
-class Solution
-{
-public:
- int searchInsert(vector<int> &nums, int target)
- {
-  int n = nums.size();
-  int low = 0, high = n - 1;
-  if (target > nums[n - 1])
-  {
-   return n;
-  }
-  while (low <= high)
-  {
-   int mid = low + (high - low) / 2;
-   cout << "low: " << low << " mid: " << mid << " high: " << high << endl;
-   if (target == nums[mid])
-   {
-    return mid; // if found return its position
-   }
-   else if (target < nums[mid])
-   {
-    high = mid - 1;
-   }
-   else if (target > nums[mid])
-   {
-    low = mid + 1;
-   }
-  }
-  return low; // if not found return the location where it should be
- }
-};
-
-//! Lower Bound STL
-class Solution
-{
-public:
- int searchInsert(vector<int> &nums, int target)
- {
-  int index = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-  return index;
- }
+    int searchInsert(vector<int> &nums, int target)
+    {
+        //* basically it's lower bound, find i where nums[i] >= target
+        int low = 0, high = nums.size() - 1;
+        int ans = nums.size();
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (nums[mid] >= target)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
 };
