753e9f99dad496a8d448142c51cb762069a2ac59
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-10-01 21:44:17 +0530

    33. Search in Rotated Sorted Array

--- Leetcode/Medium/33. Search in Rotated Sorted Array.cpp
+++ Leetcode/Medium/33. Search in Rotated Sorted Array.cpp
@@ -4,51 +4,56 @@
 #include <climits>
 using namespace std;
 
+// # Tutorial: https://www.youtube.com/watch?v=5qGrJbHhqFs&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=10
+
+/*
+> Time Complexity: O(log (n) base 2)
+> Space Complexity: O()
+*/
+
 class Solution
 {
 public:
- int search(vector<int> &nums, int target)
- {
-  int start = 0;
-  int end = nums.size() - 1;
-
-  // performing binary search
-  while (start <= end)
-  {
-   int mid = start + (end - start) / 2;
-   if (nums[mid] == target)
-   {
-    return mid;
-   }
-
-   // now we find out which halve is sorted
-
-   // if first halve is sorted
-   else if (nums[mid] >= nums[start])
-   {
-    if (target >= nums[start] && target <= nums[mid])
+    int search(vector<int> &nums, int target)
     {
-     end = mid - 1;
-    }
-    else
-    {
-     start = mid + 1;
-    }
-   }
+        //* key is to identify the sorted half
+        int low = 0, high = nums.size() - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
 
-   // if second halve is sorted
-   else if (nums[end] >= nums[mid])
-   {
-    if (target >= nums[mid] && target <= nums[end])
-    {
-     start = mid + 1;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+
+            //* left half is sorted
+            if (nums[low] <= nums[mid])
+            {
+                //* figure out if element lies in left sorted half
+                if (nums[low] <= target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            //* right half is sorted
+            else
+            {
+                //* figure out if element lies in right sorted half
+                if (nums[mid] <= target && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
-    else
-    {
-     end = mid - 1;
-    }
-   }
-  }
-  return -1;
- }
 };
