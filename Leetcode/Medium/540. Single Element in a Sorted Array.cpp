84546de33fb91b6c4e566751f0b22dd6174ac00f
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-10-02 15:11:45 +0530

    540. Single Element in a Sorted Array

--- Leetcode/Medium/540. Single Element in a Sorted Array.cpp
+++ Leetcode/Medium/540. Single Element in a Sorted Array.cpp
@@ -9,82 +9,55 @@ using namespace std;
 //! Using Binary Search
 
 /*
-> Time Complexity: O(log n)
+> Time Complexity: O(log (n) base 2)
 > Space Complexity: O(1)
-*/
-
-/*
-For odd numbers (e.g., 1, 3, 5, 7, 9), mid^1 equals to mid - 1
-For example: 3 ^ 1 = 0011 ^ 0001 = 0010 = 2
-
-For even numbers (e.g., 2, 4, 6, 8, 10), mid^1 equals to mid + 1
-For example: 6 ^ 1 = 0110 ^ 0001 = 0111 = 7
-*/
-
-/*
-Optimal Approach: (Binary Search)
-
-We will use the fact that the vector is sorted.
-
-Observation: If you divide the array in two parts, PART A: where elements are before target element and PART B: where elements are after target element then:
-
-a. In PART A, the first instance of element occurs at even index and the second instance of the element occurs at odd index.
-
-b. In PART B, the first instance of element occurs at odd index and the second instance of the element occurs at even index.
-
-c. You can visualize using following example:
-
-index:   0 1 2 3 4 5 6 7 8
-vector: [1,1,2,3,3,4,4,8,8]
-             👆 (Target element is 2 since it appears once)
-In this example, before 2 every first instance of element occurs at even index and second instance at odd index (see element 1) and after 2 every first instance of element occurs at odd index and second instance at even index (see element 3, 4 and 8).
-
-So from the above observation we will apply binary search on our array, and if we are on the PART - B side of array, we go to left, otherwise we go right.
 */
 
 class Solution
 {
 public:
- int singleNonDuplicate(vector<int> &nums)
- {
-  int low = 0, high = nums.size() - 2;
-  //* in the loop, we are checking nums[mid] == nums[mid^1] which can be equal to nums[mid] = nums[mid+1] and if mid = nums.size()-1, then mid+1 will give you out of index error.
+    int singleNonDuplicate(vector<int> &nums)
+    {
+        //! to decide which half to eliminate we look at index of pair (N, N)
+        //! (even, odd) -> element is in right half
+        //! (odd, even) -> element is in left half
 
-  while (low <= high)
-  {
+        //* edge cases
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
 
-   int mid = low + (high - low) / 2;
+        int low = 1, high = n - 2;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
 
-   // If we are on left side, move right
-   if (nums[mid] == nums[mid ^ 1])
-    low = mid + 1;
-   // if we are on right side, move left
-   else
-    high = mid - 1;
-  }
+            //* single element found
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+            {
+                return nums[mid];
+            }
 
-  return nums[low];
- }
+            /* elimination */
+            //* (even, odd) -> we are on the left half of single element, eliminate left half
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) ||
+                (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+            {
+                low = mid + 1;
+            }
+            //* we are on the right half of single element, eliminate right half
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
 };
-
-//* Checking nums[ mid ] == nums[ mid^1 ] is equivalent to checking,
-//* (mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])
-
-// mid^1 explanation:
-// If mid is odd then (mid^1) will always be the even number before mid
-// If mid is even then (mid^1) will always be the odd number after mid
-
-// And hence I am able to check whether mid is in left half or right half as:
-
-// Suppose mid is odd: then mid^1 will give even number before it,
-// then if nums[mid] == nums[mid^1]
-// then 1st instance at even (mid^1) and 2nd instance at odd (mid),
-// hence we are on left side otherwise we are on right side.
-
-// Suppose mid is even: then mid^1 will give odd number after it,
-// then if nums[mid] == nums[mid^1]
-// then 1st instance at even (mid) and 2nd instance at odd (mid^1),
-// hence we are on left side otherwise we are on right side
 
 //! Using XOR
 
@@ -96,13 +69,13 @@ public:
 class Solution
 {
 public:
- int singleNonDuplicate(vector<int> &nums)
- {
-  int sum = 0;
-  for (int i : nums)
-  {
-   sum ^= i;
-  }
-  return sum;
- }
+    int singleNonDuplicate(vector<int> &nums)
+    {
+        int sum = 0;
+        for (int i : nums)
+        {
+            sum ^= i;
+        }
+        return sum;
+    }
 };
