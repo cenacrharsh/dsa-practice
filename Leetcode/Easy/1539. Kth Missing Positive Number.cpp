bc080b115a533819058bca75208ba7d0d7ac4f37
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-10-05 23:31:04 +0530

    1539. Kth Missing Positive Number

--- Leetcode/Easy/1539. Kth Missing Positive Number.cpp
+++ Leetcode/Easy/1539. Kth Missing Positive Number.cpp
@@ -7,73 +7,58 @@
 using namespace std;
 
 //! Binary Search
+
 /*
-Intuition of this Problem:
-
-First thing is how we can find number of mission element ?, so below is the magic statement :
-arr[i] - (i + 1) : this statement will give us number of missing element before each arr[i] element, here i is nothing but mid used in the code.
-
-For example, for the array [2,3,4,7,11], the number of missing positive integers before each element is [1,1,1,3,6] using above statement.
-
-Second thing is after finding number of missing element before each element, how we can find that element which is missing ? :
-We then use binary search to find the first index start such that the number of missing positive integers before arr[start] is greater than or equal to k. Once we have found this index start, we know that the kth smallest missing positive integer is between arr[start - 1] and arr[start].
- 
-Therefore, we can compute the kth smallest missing positive integer as arr[start - 1] + (k - (arr[start - 1] - (start - 1) - 1)) = start + k.
-
-Let's break down this expression:
-
-arr[start - 1] is the last element before the kth missing positive integer, so we start with this value.
-
-(k - (arr[start - 1] - (start - 1) - 1)) is the number of missing positive integers between arr[start - 1] and the kth missing positive integer. We subtract arr[start - 1] - (start - 1) - 1 from k to account for the missing positive integers before arr[start - 1]. We then add this value to arr[start - 1] to get the kth missing positive integer.
+> Time Complexity: O(log (N) base 2)
+> Space Complexity: O(1)
 */
+
 class Solution
 {
 public:
- int findKthPositive(vector<int> &arr, int k)
- {
-  int low = 0, high = arr.size() - 1;
-  while (low <= high)
-  {
-   int mid = low + (high - low) / 2;
-   if (arr[mid] - (mid + 1) < k)
-   {
-    low = mid + 1;
-   }
-   else
-   {
-    // missing element >= k
-    high = mid - 1;
-   }
-  }
-  return low + k;
- }
+    int findKthPositive(vector<int> &arr, int k)
+    {
+        //* we'll calc the number of integers missing at each index and then apply BS to figure out k lies in b/w which 2 elements
+
+        int low = 0, high = arr.size() - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int missing = arr[mid] - (mid + 1); //* considering 0 based indexing at index i, (i+1)th element exists
+            if (missing < k)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+
+        //* high ends up to the left of low and in some cases can even end up at index -1 eg. [4, 7, 9] k = 3
+        //* kth missing = arr[high] + more req = arr[high] + (k - missing) = arr[high] + (k - (arr[high] - (high + 1))) = high + 1 + k = low + k (at the end low ends up just after high so low = high + 1)
+        return k + high + 1; //* low + k
+    }
 };
 
-//! Own Solution
+//! Brute Force
 class Solution
 {
 public:
- int findKthPositive(vector<int> &arr, int k)
- {
-  unordered_map<int, int> m;
-  for (auto it : arr)
-  {
-   m[it]++;
-  }
-  int count = 0, ans = 0, i = 1;
-  while (true)
-  {
-   if (m.find(i) == m.end())
-   {
-    count++;
-    if (count == k)
+    int findKthPositive(vector<int> &arr, int k)
     {
-     ans = i;
-     break;
+        //* every integer < k will take one place
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] <= k)
+            {
+                k++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return k;
     }
-   }
-   i++;
-  }
-  return ans;
- }
 };
