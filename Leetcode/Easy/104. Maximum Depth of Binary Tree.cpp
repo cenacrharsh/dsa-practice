a529b8bf1857bb421aeca0b783fe2eb640439812
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-10-17 23:41:53 +0530

    104. Maximum Depth of Binary Tree

--- Leetcode/Easy/104. Maximum Depth of Binary Tree.cpp
+++ Leetcode/Easy/104. Maximum Depth of Binary Tree.cpp
@@ -6,29 +6,33 @@
 #include <cstring>
 using namespace std;
 
+// # Tutorial: https://www.youtube.com/watch?v=eD3tmO66aBA&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=16
+
+/*
+> Time Complexity: O(N)
+> Space Complexity: O(N)
+*/
+
 struct TreeNode
 {
- int val;
- TreeNode *left;
- TreeNode *right;
- TreeNode() : val(0), left(nullptr), right(nullptr) {}
- TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution
 {
 public:
- int maxDepth(TreeNode *root)
- {
-  if (root == NULL)
-  {
-   return 0;
-  }
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
 
-  int leftSubtreeHeight = maxDepth(root->left);
-  int rightSubtreeHeight = maxDepth(root->right);
-
-  return max(leftSubtreeHeight, rightSubtreeHeight) + 1;
- }
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
 };
