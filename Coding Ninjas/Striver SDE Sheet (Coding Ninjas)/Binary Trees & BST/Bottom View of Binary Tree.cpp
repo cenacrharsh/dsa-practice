778ab7d57685b1df1031cedbbeb9384275824f17
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-10-22 12:58:24 +0530

    Bottom View of Binary Tree

--- Coding Ninjas/Striver SDE Sheet (Coding Ninjas)/Binary Trees & BST/Bottom View of Binary Tree.cpp
+++ Coding Ninjas/Striver SDE Sheet (Coding Ninjas)/Binary Trees & BST/Bottom View of Binary Tree.cpp
@@ -19,3 +19,48 @@ struct Node
     Node *left;
     Node *right;
 };
+
+class Solution
+{
+public:
+    vector<int> bottomView(Node *root)
+    {
+        //* last node in each vertical will be our bottom view of binary tree
+        vector<int> ans;
+        if (root == NULL)
+        {
+            return ans;
+        }
+
+        map<int, int> m; //* (vertical, node->val) to store first node of each vertical
+        queue<pair<Node *, int>> q; //* node, vertical
+
+        q.push({root, 0});
+        while (!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+
+            Node *node = it.first;
+            int vertical = it.second;
+
+            //* since we need last node in each vertical we'll replace everytime we get a new node in vertical
+            m[vertical] = node->data;
+
+            if (node->left != NULL)
+            {
+                q.push({node->left, vertical - 1});
+            }
+            if (node->right != NULL)
+            {
+                q.push({node->right, vertical + 1});
+            }
+        }
+        //* since it's a map it's already sorted as -2 -1 0 1 2...
+        for (auto it : m)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
