53de8d1a092e7c444f5d8b1824ecff39cafbdf93
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-10-28 12:27:47 +0530

    662. Maximum Width of Binary Tree

--- Leetcode/Medium/662. Maximum Width of Binary Tree.cpp
+++ Leetcode/Medium/662. Maximum Width of Binary Tree.cpp
@@ -6,7 +6,7 @@
 #include <cstring>
 using namespace std;
 
-// # Tutorial: https://www.youtube.com/watch?v=ZbybYvcVLks
+// # Tutorial: https://www.youtube.com/watch?v=ZbybYvcVLks&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=33
 
 struct TreeNode
 {
@@ -28,42 +28,49 @@ class Solution
 public:
     int widthOfBinaryTree(TreeNode *root)
     {
-        //* width: no. of nodes in a level b/w any 2 nodes
-        //* if root is i -> left: 2i + 1, right: 2i + 2
+        //* root = i, left child = (2 * i) + 1, right child = (2 * i) + 2
+        //* to prevent overflow we'll take (i - min on that level) instead of i
+        //* width = max distance b/w any 2 nodes on a level, using formula (max - min + 1)  on any level
+
         if (root == NULL)
         {
             return 0;
         }
-        int maxWidth = 0;
-        queue<pair<TreeNode *, int>> nodesLeft;
-        nodesLeft.push({root, 0});
-        while (!nodesLeft.empty())
+
+        int ans = 0;
+        queue<pair<TreeNode *, int>> q; //* (node, index)
+        q.push({root, 0});
+        while (!q.empty())
         {
-            int size = nodesLeft.size();
-            int minIndex = nodesLeft.front().second; //* to make id start from 0 in each level and prevent overflow
+            int size = q.size();
+            int minIndex = q.front().second; //* to make id start from 0 in each level and prevent overflow
             int first, last;
             for (int i = 0; i < size; i++)
             {
-                int curr_id = nodesLeft.front().second - minIndex;
-                TreeNode *currNode = nodesLeft.front().first;
-                nodesLeft.pop();
+                TreeNode *currNode = q.front().first;
+                int currIndex = q.front().second - minIndex;
+                q.pop();
 
                 if (i == 0)
-                    first = curr_id;
+                {
+                    first = currIndex;
+                }
                 if (i == size - 1)
-                    last = curr_id;
+                {
+                    last = currIndex;
+                }
 
                 if (currNode->left != NULL)
                 {
-                    nodesLeft.push({currNode->left, (long long)2 * curr_id + 1});
+                    q.push({currNode->left, (long long)2 * currIndex + 1});
                 }
                 if (currNode->right != NULL)
                 {
-                    nodesLeft.push({currNode->right, (long long)2 * curr_id + 2});
+                    q.push({currNode->right, (long long)2 * currIndex + 2});
                 }
             }
-            maxWidth = max(maxWidth, last - first + 1);
+            ans = max(ans, last - first + 1);
         }
-        return maxWidth;
+        return ans;
     }
 };
