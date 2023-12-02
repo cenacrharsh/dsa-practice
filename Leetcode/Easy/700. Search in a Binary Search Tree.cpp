2d0b77f5fb1c54631dc30560d66301d202596053
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-12-02 11:46:39 +0530

    700. Search in a Binary Search Tree

--- Leetcode/Easy/700. Search in a Binary Search Tree.cpp
+++ Leetcode/Easy/700. Search in a Binary Search Tree.cpp
@@ -23,6 +23,21 @@ class Solution
 public:
     TreeNode *searchBST(TreeNode *root, int val)
     {
+        while (root != NULL && root->val != val)
+        {
+            root = val < root->val ? root->left : root->right;
+        }
+        return root;
+    }
+};
+
+//! or
+
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
         if (root == NULL)
         {
             return NULL;
