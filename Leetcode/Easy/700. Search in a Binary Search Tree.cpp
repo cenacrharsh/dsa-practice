b92583bf2b4fdd87ccd0f85debb31f58bdb52872
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-12-02 11:43:34 +0530

    700. Search in a Binary Search Tree

--- Leetcode/Easy/700. Search in a Binary Search Tree.cpp
+++ Leetcode/Easy/700. Search in a Binary Search Tree.cpp
@@ -6,36 +6,38 @@
 #include <cstring>
 using namespace std;
 
+// # Tutorial: https://www.youtube.com/watch?v=KcNt6v_56cc&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=41
+
 struct TreeNode
 {
- int val;
- TreeNode *left;
- TreeNode *right;
- TreeNode() : val(0), left(nullptr), right(nullptr) {}
- TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution
 {
 public:
- TreeNode *searchBST(TreeNode *root, int val)
- {
-  if (root == NULL)
-  {
-   return NULL;
-  }
-  if (root->val == val)
-  {
-   return root;
-  }
-  if (val > root->val)
-  {
-   return searchBST(root->right, val);
-  }
-  else
-  {
-   return searchBST(root->left, val);
-  }
- }
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (root == NULL)
+        {
+            return NULL;
+        }
+        if (root->val == val)
+        {
+            return root;
+        }
+        if (val > root->val)
+        {
+            return searchBST(root->right, val);
+        }
+        else
+        {
+            return searchBST(root->left, val);
+        }
+    }
 };
