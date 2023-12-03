471484a655a2a9aa6ce84ebd44b5463a233d5eb9
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-12-03 18:13:19 +0530

    450. Delete Node in a BST

--- Leetcode/Medium/450. Delete Node in a BST.cpp
+++ Leetcode/Medium/450. Delete Node in a BST.cpp
@@ -6,68 +6,80 @@
 #include <cstring>
 using namespace std;
 
-//# Tutorial: https://www.youtube.com/watch?v=5_AZcOOc-kM
+// # Tutorial: https://www.youtube.com/watch?v=kouxiP_H5WE&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=45
 
 struct TreeNode
 {
- int val;
- TreeNode *left;
- TreeNode *right;
- TreeNode() : val(0), left(nullptr), right(nullptr) {}
- TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution
 {
 public:
- int max(TreeNode *root)
- {
-  if (root->right != NULL)
-  {
-   return max(root->right);
-  }
-  else
-  {
-   return root->val;
-  }
- }
- TreeNode *deleteNode(TreeNode *root, int key)
- {
-  if (root == NULL)
-  {
-   return NULL;
-  }
-  if (key > root->val)
-  {
-   root->right = deleteNode(root->right, key);
-  }
-  else if (key < root->val)
-  {
-   root->left = deleteNode(root->left, key);
-  }
-  else
-  {
-   if (root->left != NULL && root->right != NULL)
-   {
-    int lmax = max(root->left);
-    root->val = lmax;
-    root->left = deleteNode(root->left, lmax);
-    return root;
-   }
-   else if (root->left != NULL)
-   {
-    return root->left;
-   }
-   else if (root->right != NULL)
-   {
-    return root->right;
-   }
-   else
-   {
-    return NULL;
-   }
-  }
-  return root;
- }
+    TreeNode *findLastRight(TreeNode *root)
+    {
+        if (root->right == NULL)
+        {
+            return root;
+        }
+        return findLastRight(root->right);
+    }
+    TreeNode *helper(TreeNode *root)
+    {
+        if (root->left == NULL)
+        {
+            return root->right;
+        }
+        if (root->right == NULL)
+        {
+            return root->left;
+        }
+        TreeNode *rightChild = root->right;
+        TreeNode *lastRight = findLastRight(root->left);
+        lastRight->right = rightChild; //* connect the right subtree to the maximum value in left subtree
+        return root->left;
+    }
+    TreeNode *deleteNode(TreeNode *root, int key)
+    {
+        if (root == NULL)
+        {
+            return NULL;
+        }
+        if (root->val == key)
+        {
+            return helper(root);
+        }
+        TreeNode *curr = root;
+        while (curr != NULL)
+        {
+            if (key < curr->val)
+            {
+                if (curr->left != NULL && curr->left->val == key)
+                {
+                    curr->left = helper(curr->left);
+                }
+                else
+                {
+                    curr = curr->left;
+                }
+            }
+            else
+            {
+                if (curr->right != NULL && curr->right->val == key)
+                {
+                    curr->right = helper(curr->right);
+                }
+                else
+                {
+                    curr = curr->right;
+                }
+            }
+        }
+        return root;
+    }
 };
