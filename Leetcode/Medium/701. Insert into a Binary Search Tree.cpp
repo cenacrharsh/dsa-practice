2a932e0385166d9650a49684e2732a1e0ff8c3f4
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-12-03 17:52:43 +0530

    701. Insert into a Binary Search Tree

--- Leetcode/Medium/701. Insert into a Binary Search Tree.cpp
+++ Leetcode/Medium/701. Insert into a Binary Search Tree.cpp
@@ -6,6 +6,8 @@
 #include <cstring>
 using namespace std;
 
+// # Tutorial: https://www.youtube.com/watch?v=FiFiNvM29ps&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=47
+
 /*
 > Time Complexity: O(log2 (n))
 > Space Complexity: O()
@@ -13,97 +15,97 @@ using namespace std;
 
 struct TreeNode
 {
- int val;
- TreeNode *left;
- TreeNode *right;
- TreeNode() : val(0), left(nullptr), right(nullptr) {}
- TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 //! Recursive
 class Solution
 {
 public:
- TreeNode *insertIntoBST(TreeNode *root, int val)
- {
-  TreeNode *newNode = new TreeNode(val);
+    TreeNode *insertIntoBST(TreeNode *root, int val)
+    {
+        TreeNode *newNode = new TreeNode(val);
 
-  if (root == NULL)
-  {
-   root = newNode;
-  }
-  else if (val < root->val)
-  {
-   // we know we have to go left
-   if (root->left != NULL)
-   {
-    insertIntoBST(root->left, val);
-   }
-   else
-   {
-    root->left = newNode;
-   }
-  }
-  else if (val > root->val)
-  {
-   // we know we have to go right
-   if (root->right != NULL)
-   {
-    insertIntoBST(root->right, val);
-   }
-   else
-   {
-    root->right = newNode;
-   }
-  }
+        if (root == NULL)
+        {
+            root = newNode;
+        }
+        else if (val < root->val)
+        {
+            //* we know we have to go left
+            if (root->left != NULL)
+            {
+                insertIntoBST(root->left, val);
+            }
+            else
+            {
+                root->left = newNode;
+            }
+        }
+        else if (val > root->val)
+        {
+            //* we know we have to go right
+            if (root->right != NULL)
+            {
+                insertIntoBST(root->right, val);
+            }
+            else
+            {
+                root->right = newNode;
+            }
+        }
 
-  return root;
- }
+        return root;
+    }
 };
 
 //! Iterative
 class Solution
 {
 public:
- TreeNode *insertIntoBST(TreeNode *root, int val)
- {
-  if (root == NULL)
-  {
-   return new TreeNode(val);
-  }
+    TreeNode *insertIntoBST(TreeNode *root, int val)
+    {
+        if (root == NULL)
+        {
+            return new TreeNode(val);
+        }
 
-  TreeNode *temp = root;
-  while (true)
-  {
-   if (val < temp->val)
-   {
-    // we have to move left
-    if (temp->left != NULL)
-    {
-     temp = temp->left;
-    }
-    else
-    {
-     temp->left = new TreeNode(val);
-     break;
-    }
-   }
-   else
-   {
-    // we have to move right
-    if (temp->right != NULL)
-    {
-     temp = temp->right;
-    }
-    else
-    {
-     temp->right = new TreeNode(val);
-     break;
-    }
-   }
-  }
+        TreeNode *temp = root;
+        while (true)
+        {
+            if (val < temp->val)
+            {
+                //* we have to move left
+                if (temp->left != NULL)
+                {
+                    temp = temp->left;
+                }
+                else
+                {
+                    temp->left = new TreeNode(val);
+                    break;
+                }
+            }
+            else
+            {
+                //* we have to move right
+                if (temp->right != NULL)
+                {
+                    temp = temp->right;
+                }
+                else
+                {
+                    temp->right = new TreeNode(val);
+                    break;
+                }
+            }
+        }
 
-  return root;
- }
+        return root;
+    }
 };
