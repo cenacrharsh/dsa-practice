3980b9793456742015ee24a80234e572873d0575
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-12-06 22:01:47 +0530

    98. Validate Binary Search Tree

--- Leetcode/Medium/98. Validate Binary Search Tree.cpp
+++ Leetcode/Medium/98. Validate Binary Search Tree.cpp
@@ -8,87 +8,65 @@ using namespace std;
 
 struct TreeNode
 {
- int val;
- TreeNode *left;
- TreeNode *right;
- TreeNode() : val(0), left(nullptr), right(nullptr) {}
- TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-//! All Parent Nodes Lie In A Range
+//! All Elements Lie In A Range
 class Solution
 {
 public:
- bool helper(TreeNode *root, long minValue, long maxValue)
- {
-  if (root == NULL)
-   return true;
+    bool helper(TreeNode *root, long minValue, long maxValue)
+    {
+        if (root == NULL)
+        {
+            return true;
+        }
 
-  if (root->val <= minValue || root->val >= maxValue)
-   return false;
+        if (root->val <= minValue || root->val >= maxValue)
+        {
+            return false;
+        }
 
-  return (helper(root->left, minValue, root->val) &&
-          helper(root->right, root->val, maxValue));
- }
-
- bool isValidBST(TreeNode *root)
- {
-  return helper(root, LONG_MIN, LONG_MAX);
- }
-};
-
-//* or
-class Solution
-{
-public:
- static bool isValidBSTHelper(TreeNode *root, long min, long max)
- {
-  if (root == NULL)
-  {
-   return true;
-  }
-
-  if (root->val <= min || root->val >= max)
-  {
-   return false;
-  }
-
-  return (isValidBSTHelper(root->left, min, root->val) && isValidBSTHelper(root->right, root->val, max));
- }
-
- bool isValidBST(TreeNode *root)
- {
-  return isValidBSTHelper(root, LONG_MIN, LONG_MAX);
- }
+        return (helper(root->left, minValue, root->val) &&
+                helper(root->right, root->val, maxValue));
+    }
+    bool isValidBST(TreeNode *root)
+    {
+        return helper(root, LONG_MIN, LONG_MAX);
+    }
 };
 
 //! Inorder Traversal
 
 class Solution
 {
- long prev = LONG_MIN;
+    long prev = LONG_MIN;
 
 public:
- bool isValidBST(TreeNode *root)
- {
-  if (root == NULL)
-  {
-   return true;
-  }
+    bool isValidBST(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return true;
+        }
 
-  if (!isValidBST(root->left))
-  {
-   return false;
-  }
+        if (!isValidBST(root->left))
+        {
+            return false;
+        }
 
-  if (root->val <= prev)
-  {
-   return false;
-  }
+        if (root->val <= prev)
+        {
+            return false;
+        }
 
-  prev = root->val;
+        prev = root->val;
 
-  return isValidBST(root->right);
- }
+        return isValidBST(root->right);
+    }
 };
