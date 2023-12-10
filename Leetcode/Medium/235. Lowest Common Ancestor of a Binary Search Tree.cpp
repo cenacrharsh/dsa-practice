ead065cc28626b1df6dea375e5d2565b7f19430a
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-12-10 10:51:10 +0530

    235. Lowest Common Ancestor of a Binary Search Tree

--- Leetcode/Medium/235. Lowest Common Ancestor of a Binary Search Tree.cpp
+++ Leetcode/Medium/235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -6,46 +6,88 @@
 #include <cstring>
 using namespace std;
 
-// # Tutorial: https://www.youtube.com/watch?v=_-QHfMDde90
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// # Tutorial: https://www.youtube.com/watch?v=cX_kPV_foZc&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=52
+
+//! BST Approach
+
+/*
+> Time Complexity: O(H)
+> Space Complexity: O(n) => auxiliary stack space
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        //* if both nodes lie on left wo move left, if both lie on right we move right but if neither case then it means one node is in left and one right or the root itself is one of the nodes and in either case we have found the LCA
+        if (root == NULL)
+        {
+            return NULL;
+        }
+        //* check if both nodes lie on left or right
+        if ((p->val < root->val) && (q->val < root->val))
+        {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        if ((p->val > root->val) && (q->val > root->val))
+        {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        return root;
+    }
+};
+
+//! Binary Tree Approach
 
 /*
 > Time Complexity: O(n)
 > Space Complexity: O(n) => auxiliary stack space
 */
 
-struct TreeNode
-{
- int val;
- TreeNode *left;
- TreeNode *right;
- TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 class Solution
 {
 public:
- TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
- {
-  if (root == NULL || root == p || root == q)
-  {
-   return root;
-  }
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root == NULL || root == p || root == q)
+        {
+            return root;
+        }
 
-  TreeNode *left = lowestCommonAncestor(root->left, p, q);
-  TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
 
-  if (left == NULL)
-  {
-   return right;
-  }
-  else if (right == NULL)
-  {
-   return left;
-  }
-  else
-  {
-   // both left and right are not null, we found our result
-   return root;
-  }
- }
+        if (left == NULL)
+        {
+            return right;
+        }
+        else if (right == NULL)
+        {
+            return left;
+        }
+        else
+        {
+            //* both left and right are not null, we found our result
+            return root;
+        }
+    }
 };
