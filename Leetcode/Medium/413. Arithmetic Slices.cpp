cbdf7b23debc62345c8d18272e1b3975c672a9d1
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2024-01-07 12:37:10 +0530

    413. Arithmetic Slices

--- Leetcode/Medium/413. Arithmetic Slices.cpp
+++ Leetcode/Medium/413. Arithmetic Slices.cpp
@@ -9,23 +9,26 @@ using namespace std;
 class Solution
 {
 public:
- int numberOfArithmeticSlices(vector<int> &nums)
- {
-  int n = nums.size();
-  if (n < 3)
-  {
-   return 0;
-  }
-  vector<int> dp(n, 0);
-  int count = 0;
-  for (int i = 2; i < n; i++)
-  {
-   if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2])
-   {
-    dp[i] = dp[i - 1] + 1;
-    count += dp[i];
-   }
-  }
-  return count;
- }
+    int numberOfArithmeticSlices(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n < 3)
+        {
+            return 0;
+        }
+
+        vector<int> dp(n, 0); //* dp[i] = number of Arithmetic Progressions that are ending at index i
+
+        int count = 0;
+        for (int i = 2; i < n; i++)
+        {
+            //* check if last numbers are in AP
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2])
+            {
+                dp[i] = dp[i - 1] + 1; //* if we already had an AP at previous index it will also grow and we got another AP hence we do dp[i - 1] + 1
+                count += dp[i];        //* ans is summation of all AP's
+            }
+        }
+        return count;
+    }
 };
