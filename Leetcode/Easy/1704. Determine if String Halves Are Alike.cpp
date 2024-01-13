d87cae3007ca961de0316fd0dbb3027ec3f54a5b
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2024-01-13 12:13:04 +0530

    1704. Determine if String Halves Are Alike

--- Leetcode/Easy/1704. Determine if String Halves Are Alike.cpp
+++ Leetcode/Easy/1704. Determine if String Halves Are Alike.cpp
@@ -9,37 +9,37 @@ using namespace std;
 class Solution
 {
 public:
- bool halvesAreAlike(string s)
- {
-  unordered_map<char, int> freq1, freq2;
-  int n = s.length();
-  for (int i = 0; i < n / 2; i++)
-  {
-   freq1[s[i]]++;
-  }
-  for (int i = n / 2; i < n; i++)
-  {
-   freq2[s[i]]++;
-  }
-  int count1 = 0, count2 = 0;
-  for (auto x : freq1)
-  {
-   char currChar = x.first;
-   if (currChar == 'a' || currChar == 'e' || currChar == 'i' || currChar == 'o' || currChar == 'u' || currChar == 'A' || currChar == 'E' ||
-       currChar == 'I' || currChar == 'O' || currChar == 'U')
-   {
-    count1 += freq1[currChar];
-   }
-  }
-  for (auto x : freq2)
-  {
-   char currChar = x.first;
-   if (currChar == 'a' || currChar == 'e' || currChar == 'i' || currChar == 'o' || currChar == 'u' || currChar == 'A' || currChar == 'E' ||
-       currChar == 'I' || currChar == 'O' || currChar == 'U')
-   {
-    count2 += freq2[currChar];
-   }
-  }
-  return (count1 == count2);
- }
+    bool halvesAreAlike(string s)
+    {
+        unordered_map<char, int> freq1, freq2;
+        int n = s.length();
+        for (int i = 0; i < n / 2; i++)
+        {
+            freq1[s[i]]++;
+        }
+        for (int i = n / 2; i < n; i++)
+        {
+            freq2[s[i]]++;
+        }
+        int count1 = 0, count2 = 0;
+        for (auto x : freq1)
+        {
+            char currChar = x.first;
+            if (currChar == 'a' || currChar == 'e' || currChar == 'i' || currChar == 'o' || currChar == 'u' || currChar == 'A' || currChar == 'E' ||
+                currChar == 'I' || currChar == 'O' || currChar == 'U')
+            {
+                count1 += freq1[currChar];
+            }
+        }
+        for (auto x : freq2)
+        {
+            char currChar = x.first;
+            if (currChar == 'a' || currChar == 'e' || currChar == 'i' || currChar == 'o' || currChar == 'u' || currChar == 'A' || currChar == 'E' ||
+                currChar == 'I' || currChar == 'O' || currChar == 'U')
+            {
+                count2 += freq2[currChar];
+            }
+        }
+        return (count1 == count2);
+    }
 };
