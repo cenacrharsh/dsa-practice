3029ebd4568ac452a2267b5227555cb9e73e51e6
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2024-01-30 23:11:36 +0530

    Insert at end of Doubly Linked List

---
+++ Coding Ninjas/Striver SDE Sheet (Coding Ninjas)/Linked List/Insert at end of Doubly Linked List.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node() : data(0), next(nullptr), prev(nullptr) {}
+    Node(int x) : data(x), next(nullptr), prev(nullptr) {}
+    Node(int x, Node *next, Node *prev) : data(x), next(next), prev(prev) {}
+};
+
+Node *insertAtTail(Node *head, int k)
+{
+    Node *newNode = new Node(k);
+    if (head == NULL)
+    {
+        return newNode;
+    }
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+    return head;
+}
