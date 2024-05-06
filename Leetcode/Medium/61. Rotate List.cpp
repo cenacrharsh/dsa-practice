273a329f808e8e64cedddf16f97b7e63aee1f6b5
Kumar Harsh <harsh@rubick.ai>
2024-05-06 21:48:12 +0530

    61. Rotate List

--- Leetcode/Medium/61. Rotate List.cpp
+++ Leetcode/Medium/61. Rotate List.cpp
@@ -6,57 +6,66 @@
 #include <cstring>
 using namespace std;
 
+struct ListNode
+{
+	ListNode *next;
+	int val;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 //! Optimized Approach
 
 /*
-> Time Complexity: O(n) + O(n - (n % k))
+> Time Complexity: O(N) + O(N) -> find tail + find kth node
 > Space Complexity: O(1)
 */
 
-struct ListNode
-{
- int val;
- ListNode *next;
- ListNode() : val(0), next(nullptr) {}
- ListNode(int x) : val(x), next(nullptr) {}
- ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-class Solution
-{
+class Solution {
 public:
- ListNode *rotateRight(ListNode *head, int k)
- {
-  if (head == NULL || head->next == NULL || k < 1)
-  {
-   return head;
-  }
-  ListNode *curr = head;
-  int count = 1;
-  while (curr->next != NULL)
-  {
-   count++;
-   curr = curr->next;
-  }
-  // last node points to head
-  curr->next = head;
+    ListNode* findKthNode(ListNode* node, int k) {
+        int count = 1;
+        while(node != NULL && k > 0) {
+            if(count == k) {
+                return node;
+            }
+            count++;
+            node = node->next;
+        }
+        return node;
+    }
 
-  // calc value of effective k from start of LL, which will become the last node of rotated LL and k+1 th node will become the new head of LL
-  k = count - k % count;
+    ListNode* rotateRight(ListNode* head, int k) {
+        //* find length of LL and last node, make last node point to head
+        //* find (N-K)th node and make it point to NULL, also the next node to (N-K)th node becomes the new head of the LL
 
-  // traverse and reach kth node, i.e. k - 1 traversals from head
-  curr = head;
-  for (int i = 1; i < k; i++)
-  {
-   curr = curr->next;
-  }
+        if(head == NULL || k == 0) {
+            return head;
+        }
 
-  // now update head to point to k+1 th node, and kth node points to null
-  head = curr->next;
-  curr->next = NULL;
+        //* find last node and count
+        ListNode *tail = head;
+        int count = 1;
+        while(tail->next != NULL) {
+            count++;
+            tail = tail->next;
+        }
 
-  return head;
- }
+        //* reduce k to be in range of length of LL if k > count, also if k = multiple of count we don't have to do anything
+        k = k % count;
+        if(k == 0) {
+            return head;
+        }
+        tail->next = head;
+        
+        //* find (N-K)th node
+        ListNode *newLastNode = findKthNode(head, count - k);
+        head = newLastNode->next;
+        newLastNode->next = NULL;
+
+        return head;
+    }
 };
 
 //! Brute Force - K rotations
@@ -68,27 +77,27 @@ public:
 
 class Solution
 {
-public:
- ListNode *rotateRight(ListNode *head, int k)
- {
-  if (head == NULL || head->next == NULL || k < 1)
-  {
-   return head;
-  }
+    public:
+    ListNode *rotateRight(ListNode *head, int k)
+    {
+        if (head == NULL || head->next == NULL || k < 1)
+        {
+            return head;
+        }
 
-  for (int i = 0; i < k; i++)
-  {
-   ListNode *lastNode = head;
-   ListNode *secondLastNode = NULL;
-   while (lastNode->next != NULL)
-   {
-    secondLastNode = lastNode;
-    lastNode = lastNode->next;
-   }
-   lastNode->next = head;
-   secondLastNode->next = NULL;
-   head = lastNode;
-  }
-  return head;
- }
+        for (int i = 0; i < k; i++)
+        {
+            ListNode *lastNode = head;
+            ListNode *secondLastNode = NULL;
+            while (lastNode->next != NULL)
+            {
+                secondLastNode = lastNode;
+                lastNode = lastNode->next;
+            }
+            lastNode->next = head;
+            secondLastNode->next = NULL;
+            head = lastNode;
+        }
+        return head;
+    }
 };
