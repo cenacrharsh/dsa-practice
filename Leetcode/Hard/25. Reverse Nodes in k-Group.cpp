ecc61b1d175467c072fd8e2ca8ab5cb80adaa371
Kumar Harsh <harsh@rubick.ai>
2024-05-06 21:15:31 +0530

    25. Reverse Nodes in k-Group

--- Leetcode/Hard/25. Reverse Nodes in k-Group.cpp
+++ Leetcode/Hard/25. Reverse Nodes in k-Group.cpp
@@ -6,59 +6,73 @@
 #include <cstring>
 using namespace std;
 
-//# Tutorial: https://www.youtube.com/watch?v=Of0HPkk3JgI
-
 struct ListNode
 {
- int val;
- ListNode *next;
- ListNode() : val(0), next(nullptr) {}
- ListNode(int x) : val(x), next(nullptr) {}
- ListNode(int x, ListNode *next) : val(x), next(next) {}
+	ListNode *next;
+	int val;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+//# Tutorial: https://www.youtube.com/watch?v=lIar1skcQYI&list=PLgUwDviBIf0rAuz8tVcM0AymmhTRsfaLU&index=22
+
 /*
-> Time Complexity: O(n/k * k) -> O(n)
+> Time Complexity: O(N) + O(N) -> Reversal + Finding Kth Node
 > Space Complexity: O(1)
 */
 
-class Solution
-{
+class Solution {
 public:
- ListNode *reverseKGroup(ListNode *head, int k)
- {
-  if (head == NULL || k == 1)
-  {
-   return head;
-  }
+    ListNode* findKthNode(ListNode* node, int k) {
+        k = k - 1;
+        while(node != NULL && k > 0) {
+            k--;
+            node = node->next;
+        }
+        return node;
+    }
 
-  ListNode *dummy = new ListNode(0);
-  dummy->next = head; //! d->1->2->3->4
+    ListNode* reverse(ListNode* head) {
+        ListNode *prev = NULL, *curr = head, *next = NULL;
+        while(curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
 
-  ListNode *temp = head;
-  int count = 0;
-  while (temp != NULL)
-  {
-   count++;
-   temp = temp->next;
-  }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode *temp = head, *nextNode = NULL, *prevLastNode = NULL;
+        while(temp != NULL) {
+            ListNode *kthNode = findKthNode(temp, k);
+            if(kthNode == NULL) {
+                //* if LL doesn't even have one group to reverse, in that case our prevNode will be NULL and we don't have to do anything
+                if(prevLastNode != NULL) {
+                    prevLastNode->next = temp;
+                }
+                break;
+            }
 
-  ListNode *prevNode = dummy, *currNode = dummy, *nextNode = dummy;
-  while (count >= k)
-  {
-   currNode = prevNode->next;
-   nextNode = currNode->next; //* prev @ dummy, curr @ 1, next @ 2
-   for (int i = 1; i < k; i++)
-   {
-    currNode->next = nextNode->next; //* 1->3, 1->4
-    nextNode->next = prevNode->next; //* 2>1, 3->2
-    prevNode->next = nextNode;       //* d->2, d->3
-    nextNode = currNode->next;       //* next @ 3, next @ 4
-   }
-   //! d->3->2->1->4
-   prevNode = currNode; //* curr @ 1 so prev @ 1
-   count -= k;
-  }
-  return dummy->next;
- }
+            //* we can't reverse a portion of a LL so we break off this part and reverse it
+            nextNode = kthNode->next;
+            kthNode->next = NULL;
+            reverse(temp); //* we already know the head of reversed LL will be the kthNode
+
+            //* if it's the first group then we have to make sure we update the head of the LL as well
+            if(temp == head) {
+                head = kthNode;
+            } else {
+                prevLastNode->next = kthNode;
+            }
+
+            //* maintain the previous last node to reconnect the broken links in LL
+            prevLastNode = temp;
+            temp = nextNode;
+        }
+        return head;
+    }
 };
+
