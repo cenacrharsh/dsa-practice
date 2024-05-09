73a5721e68145ea78e2b527be3142816a2793023
Kumar Harsh <harsh@rubick.ai>
2024-05-09 22:09:26 +0530

    23. Merge k Sorted Lists

--- Leetcode/Hard/23. Merge k Sorted Lists.cpp
+++ Leetcode/Hard/23. Merge k Sorted Lists.cpp
@@ -8,54 +8,56 @@ using namespace std;
 
 struct ListNode
 {
- int val;
- ListNode *next;
- ListNode() : val(0), next(nullptr) {}
- ListNode(int x) : val(x), next(nullptr) {}
- ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+//! Using Priority Queue
+
+// # Tutorial: https://www.youtube.com/watch?v=1zktEppsdig&list=PLgUwDviBIf0rAuz8tVcM0AymmhTRsfaLU&index=28
+
+/*
+> Time Complexity: O(K * Log(K)) + O(N * K * Log(K))
+> Space Complexity: O(K)
+*/
 
 class Solution
 {
 public:
- ListNode *mergeKLists(vector<ListNode *> &lists)
- {
-  if (lists.size() == 0)
-   return NULL;
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        priority_queue<pair<int, ListNode *>, vector<pair<int, ListNode *>>, greater<pair<int, ListNode *>>> pq; //* value->node
 
-  ListNode *dummyNode = new ListNode(-1);
-  ListNode *currNode = dummyNode;
+        //* push heads of all given LL into min heap, TC: O(K) * O(Log(K)) where each operation on PQ takes Log(K) time where K is size of PQ
+        for (auto head : lists)
+        {
+            if (head)
+            {
+                pq.push(make_pair(head->val, head));
+            }
+        }
 
-  // min heap containing value and pointer to currNode of LL
-  priority_queue<pair<int, ListNode *>,
-                 vector<pair<int, ListNode *>>,
-                 greater<pair<int, ListNode *>>>
-      pq;
+        ListNode *dummy = new ListNode(-1);
+        ListNode *temp = dummy;
+        //* TC: (K*N) * 3Log(K), K*N is total number of elements in PQ and 3 operations on PQ in each iteration
+        while (!pq.empty())
+        {
+            //* pick smallest value from heap and add it to ans
+            ListNode *smallestNode = pq.top().second;
+            pq.pop();
 
-  // pushing first node and address of head of all LL in priority queue
-  for (auto head : lists)
-  {
-   if (head != NULL)
-   {
-    pq.push({head->val, head});
-   }
-  }
+            temp->next = smallestNode;
+            temp = temp->next;
 
-  while (!pq.empty())
-  {
-   ListNode *minNode = pq.top().second;
-   pq.pop();
-
-   // add minNode to LL
-   currNode->next = minNode;
-   currNode = currNode->next;
-
-   // add next node of curr LL in priority queue
-   if (minNode->next != NULL)
-   {
-    pq.push({minNode->next->val, minNode->next});
-   }
-  }
-  return dummyNode->next;
- }
+            //* push next node of smallest node into heap
+            if (smallestNode->next)
+            {
+                pq.push(make_pair(smallestNode->next->val, smallestNode->next));
+            }
+        }
+        return dummy->next;
+    }
 };
