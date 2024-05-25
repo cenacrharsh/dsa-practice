135f5386bf0f89738a164d8396d96f69fd42fc2a
Kumar Harsh <harsh@rubick.ai>
2024-05-25 15:47:56 +0530

    3. Longest Substring Without Repeating Characters

--- Leetcode/Medium/3. Longest Substring Without Repeating Characters.cpp
+++ Leetcode/Medium/3. Longest Substring Without Repeating Characters.cpp
@@ -6,33 +6,57 @@
 #include <cstring>
 using namespace std;
 
-class Solution
-{
+// # Tutorial: https://www.youtube.com/watch?v=-zSxTJkcdAo&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=3
+
+//! Using Freq Array
+
+/*
+> Time Complexity: O(N)
+> Space Complexity: O(256)
+*/
+
+class Solution {
 public:
- int lengthOfLongestSubstring(string s)
- {
-  if (s == "")
-  {
-   return 0;
-  }
-  unordered_set<int> unique;
-  int len = 0, maxLen = INT_MIN;
-  int l = 0, r = 0;
-  while (r < s.size())
-  {
-   if (unique.find(s[r]) != unique.end())
-   {
-    while (unique.find(s[r]) != unique.end())
-    {
-     unique.erase(s[l]);
-     l++;
+    int lengthOfLongestSubstring(string s) {
+        vector<int> freq(256, 0);
+        int left = 0, right = 0, maxLen = 0;
+        while(right < s.size()) {
+            while(freq[s[right]] != 0) {
+                freq[s[left]]--;
+                left++;
+            }
+            maxLen = max(maxLen, right - left + 1);
+            freq[s[right]]++;
+            right++;
+        }
+        return maxLen;
     }
-   }
-   unique.insert(s[r]);
-   len = r - l + 1;
-   maxLen = max(maxLen, len);
-   r++;
-  }
-  return maxLen;
- }
+};
+
+//! Using Hashmap
+
+/*
+> Time Complexity: O(N)
+> Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<int, int> freq; //* char->index
+        int left = 0, right = 0, maxLen = 0;
+        while(right < s.size()) {
+            while(freq.find(s[right]) != freq.end()) {
+                freq[s[left]]--;
+                if(freq[s[left]] == 0) {
+                    freq.erase(s[left]);
+                }
+                left++;
+            }
+            maxLen = max(maxLen, right - left + 1);
+            freq[s[right]]++;
+            right++;
+        }
+        return maxLen;
+    }
 };
