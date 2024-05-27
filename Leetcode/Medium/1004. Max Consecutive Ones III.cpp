f5eb065a47ee811494e8cd918db1540e21cb23f7
Kumar Harsh <harsh@rubick.ai>
2024-05-27 23:04:47 +0530

    1004. Max Consecutive Ones III

--- Leetcode/Medium/1004. Max Consecutive Ones III.cpp
+++ Leetcode/Medium/1004. Max Consecutive Ones III.cpp
@@ -6,29 +6,73 @@
 #include <cstring>
 using namespace std;
 
-//# Tutorial: https://www.youtube.com/watch?v=QPfalDbqa4A
+//# Tutorial: https://www.youtube.com/watch?v=3E4JBHSLpYk&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=9
 
-class Solution
-{
+//! Optimized Sliding Window
+
+/*
+> Time Complexity: O(N)
+> Space Complexity: O(1)
+*/
+
+class Solution {
 public:
- int longestOnes(vector<int> &nums, int k)
- {
-  int zeroes = 0, ans = 0, j = 0;
-  for (int i = 0; i < nums.size(); i++)
-  {
-   if (nums[i] == 0)
-    zeroes++;
+    int longestOnes(vector<int>& nums, int k) {
+        //* longest subarray with at most k zeroes
+        //* optimized sliding window we don't let window size drop below the max size we have found already as that's our ans if we don't find anything bigger than that so no point in shrinking window and then increasing it again, thus removing the inner while loop
 
-   while (zeroes > k)
-   {
-    if (nums[j] == 0)
-     zeroes--;
-    j++;
-   }
+        int left = 0, right = 0, numZeroes = 0, maxLen = INT_MIN;
+        while(right < nums.size()) {
+            if(nums[right] == 0) {
+                numZeroes++;
+            }
 
-   int len = i - j + 1;
-   ans = max(ans, len);
-  }
-  return ans;
- }
+            if(numZeroes > k) {
+                if(nums[left] == 0) {
+                    numZeroes--;
+                }
+                left++;
+            }
+
+            if(numZeroes <= k) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+            right++;
+        }
+        return maxLen;
+    }
 };
+
+//! Sliding Window
+
+/*
+> Time Complexity: O(N) + O(N) -> both while loops can traverse at max N
+> Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        //* longest subarray with at most k zeroes
+        int left = 0, right = 0, numZeroes = 0, maxLen = INT_MIN;
+        while(right < nums.size()) {
+            if(nums[right] == 0) {
+                numZeroes++;
+            }
+
+            while(numZeroes > k) {
+                if(nums[left] == 0) {
+                    numZeroes--;
+                }
+                left++;
+            }
+            
+            if(numZeroes <= k) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+            right++;
+        }
+        return maxLen;
+    }
+};
+
