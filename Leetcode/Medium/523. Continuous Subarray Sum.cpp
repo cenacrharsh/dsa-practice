6d42b5504f15583ef87bc050addb1378d650c5e7
Kumar Harsh <harsh@rubick.ai>
2024-06-08 18:26:45 +0530

    523. Continuous Subarray Sum

--- Leetcode/Medium/523. Continuous Subarray Sum.cpp
+++ Leetcode/Medium/523. Continuous Subarray Sum.cpp
@@ -6,52 +6,35 @@
 #include <cstring>
 using namespace std;
 
-/*
-for subarray sum of (3, 5) -> (0, 5) - (0, 2), so sum1 - sum2 will give us a subarray sum and % k = 0 indicates that sum is a multiple fo k
-(sum1 - sum2) % k = 0
-sum1 % k = sum2 % k
-use hashmap of sum -> index
-store sum = sum % k
-once we find a sum we have seen already, we know that the subarray sum if a multiple of k, just chk is size is atleast 2
-*/
-
-//# Tutorial: https://www.youtube.com/watch?v=OKcrLfR-8mE, https://www.youtube.com/watch?v=wsTcByj8QbI
+// # Tutorial: https://www.youtube.com/watch?v=1W_HYBqvDLw
 
 /*
-> Time Complexity: O(n)
-> Space Complexity: O(n)
+> Time Complexity: O(N)
+> Space Complexity: O(N)
 */
 
-class Solution
-{
+class Solution {
 public:
- bool checkSubarraySum(vector<int> &nums, int k)
- {
-  unordered_map<int, int> prefixSumRemainderMap; //* sum -> index
-  prefixSumRemainderMap[0] = -1;
-  int prefixSum = 0;
-  for (int i = 0; i < nums.size(); i++)
-  {
-   prefixSum += nums[i];
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        //* prefix sum b/w index i & j = prefixSum(j) - prefixSum(i) & if this is divisible by k then we have our good subarray
+        //* [prefixSum(j) - prefixSum(i)] % k == 0 => prefixSum(i)] % k == prefixSum(j) % k
+        //* we'll maintain a prefixSumRemainder map and whereever we have same values we may conclude that the sum of range b/w these 2 occurrences will give, sum % k == 0, also to maintain len >= 2 we'll always look for first and last occurence of that remainder in the map
 
-   if (k != 0)
-   {
-    prefixSum = prefixSum % k;
-   }
+        int prefixSumRemainder = 0;
+        unordered_map<int, int> prefixSumRemainderMap; //* prefixSum->first occurence index
+        prefixSumRemainderMap[0] = -1;
+        for(int i = 0; i < nums.size(); i++) {
+            prefixSumRemainder = (prefixSumRemainder + nums[i]) % k;
 
-   if (prefixSumRemainderMap.find(prefixSum) != prefixSumRemainderMap.end())
-   {
-    // (3, 5) -> (0, 5) - (0, 2), no of elements b/w 3->5 is 3 i.e. 5 - 2 = 3
-    if ((i - prefixSumRemainderMap[prefixSum]) >= 2)
-    {
-     return true;
+            //* if the current prefixSumRemainder is already present in out hashmap we'll see if the length of subarray >= 2 or not
+            if(prefixSumRemainderMap.find(prefixSumRemainder) != prefixSumRemainderMap.end()) {
+                if(i - prefixSumRemainderMap[prefixSumRemainder] >= 2) {
+                    return true;
+                }
+            } else {
+                prefixSumRemainderMap[prefixSumRemainder] = i;
+            }
+        }
+        return false;
     }
-   }
-   else
-   {
-    prefixSumRemainderMap[prefixSum] = i;
-   }
-  }
-  return false;
- }
 };
