a8b6a9111dcd62dea525390b559b56ce7f8b4a31
Kumar Harsh <harsh@rubick.ai>
2024-06-12 02:26:50 +0530

    231. Power of Two

--- Leetcode/Easy/231. Power of Two.cpp
+++ Leetcode/Easy/231. Power of Two.cpp
@@ -6,45 +6,15 @@
 #include <cstring>
 using namespace std;
 
-// # Tutorial: https://www.youtube.com/watch?v=I80Ovvi_nRU
+// # Tutorial: https://www.youtube.com/watch?v=nttpF8kwgd4&list=PLgUwDviBIf0rnqh8QsJaHyIX7KUiaPUv7&index=4
 
 //! Bit Manipulation
-class Solution
-{
+
+class Solution {
 public:
- bool isPowerOfTwo(int n)
- {
-  // 1 << x = 2^x = 1 000..(x o's from 2^0 -> 2^x-1 and 1 is the 2^xth one)
-  // n << x = n * 2^x ( i.e multiply the num with 2^x)
-  // n >> x = n / 2^x ( i.e divide the num by 2^x)
+    bool isPowerOfTwo(int n) {
+        //* binary representation of a number which is a power of 2 will only have 1 set bit, so doing N & (N-1) would result in a 0 if only 1 set bit is present in the number because N & (N - 1) unsets the rightmost set bit and everything remains as it is in this case everything was 0 already
 
-  // n       = 1 000
-  // n-1     = 0 111
-  // n & n-1 = 0 000, so if n & n - 1 is 0, n is power of 2
-
-  if (n <= 0)
-  {
-   return false;
-  }
-  return (n & n - 1) == 0;
- }
-};
-
-//! By generating powers and checking
-class Solution
-{
-public:
- bool isPowerOfTwo(int n)
- {
-  long powersOfTwo = 1; // we start with the first one 2^0 = 1
-
-  // we keep generating powers of 2 until it's < n
-  while (powersOfTwo < n)
-  {
-   powersOfTwo *= 2;
-  }
-
-  // check if i == n, if not we can't generate any more powers as it'll cross n
-  return powersOfTwo == n;
- }
+        return (n > 0 && (n & (n - 1))  == 0 ) ? true : false;
+    }
 };
