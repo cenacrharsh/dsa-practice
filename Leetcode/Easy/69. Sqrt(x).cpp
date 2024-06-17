711d48b40884174e11cbecbaca767cf09b1a1710
Kumar Harsh <harsh@rubick.ai>
2024-06-17 23:58:08 +0530

    69. Sqrt(x)

--- Leetcode/Easy/69. Sqrt(x).cpp
+++ Leetcode/Easy/69. Sqrt(x).cpp
@@ -5,33 +5,35 @@
 #include <unordered_map>
 using namespace std;
 
-class Solution
-{
+/*
+> Time Complexity: O(Log(N))
+> Space Complexity: O(1)
+*/
+
+class Solution {
 public:
- int mySqrt(int x)
- {
-  if (x == 0)
-  {
-   return 0;
-  }
+    int mySqrt(int x) {
+        if(x == 0) {
+            return 0;
+        }
+        
+        int low = 1, high = x;
+        int sqRoot = 1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
 
-  int low = 1;
-  int high = x;
-  int sqRoot = 1;
-
-  while (low <= high)
-  {
-   int mid = low + (high - low) / 2;
-   if (mid <= x / mid)
-   {
-    low = mid + 1;
-    sqRoot = mid;
-   }
-   else
-   {
-    high = mid - 1;
-   }
-  }
-  return sqRoot;
- }
+            //* not using mid * mid <= x to prevent integer overflow
+            if(mid <= x / mid) {
+                low = mid + 1;
+                sqRoot = mid;
+            }
+            else {
+                high = mid - 1;
+            }
+            
+        }
+        return sqRoot;
+    }
 };
