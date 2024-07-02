83f04abb32f6d2a9eaf71d5e6bc45eb1de1bacc9
Kumar Harsh <harsh@rubick.ai>
2024-07-02 22:03:01 +0530

    2540. Minimum Common Value

--- Leetcode/Easy/2540. Minimum Common Value.cpp
+++ Leetcode/Easy/2540. Minimum Common Value.cpp
@@ -4,34 +4,27 @@
 #include <climits>
 #include <unordered_map>
 #include <cstring>
+#include <unordered_set>
 using namespace std;
 
 //! Using 2 Pointers
 
 /*
-> Time Complexity: O(N)
+> Time Complexity: O(N + M)
 > Space Complexity: O(1)
 */
 
-class Solution
-{
+class Solution {
 public:
-    int getCommon(vector<int> &nums1, vector<int> &nums2)
-    {
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int i = 0, j = 0;
-        while (i < nums1.size() && j < nums2.size())
-        {
-            if (nums1[i] == nums2[j])
-            {
+        while(i < nums1.size() && j < nums2.size()) {
+            if(nums1[i] == nums2[j]) {
                 return nums1[i];
-            }
-            else if (nums1[i] < nums2[j])
-            {
-                i++;
-            }
-            else if (nums1[i] > nums2[j])
-            {
+            } else if(nums1[i] > nums2[j]) {
                 j++;
+            } else {
+                i++;
             }
         }
         return -1;
@@ -41,24 +34,16 @@ public:
 //! Using Unordered Map
 
 /*
-> Time Complexity: O(N)
+> Time Complexity: O(N + M)
 > Space Complexity: O(N)
 */
 
-class Solution
-{
+class Solution {
 public:
-    int getCommon(vector<int> &nums1, vector<int> &nums2)
-    {
-        unordered_map<int, int> freq;
-        for (int num : nums1)
-        {
-            freq[num]++;
-        }
-        for (int num : nums2)
-        {
-            if (freq.find(num) != freq.end())
-            {
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        for(int num : nums2) {
+            if(set1.find(num) != set1.end()) {
                 return num;
             }
         }
