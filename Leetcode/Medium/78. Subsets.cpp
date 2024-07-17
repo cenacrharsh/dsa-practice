349938ed88319e973e764248e7e624106dfadbb5
Kumar Harsh <harsh@rubick.ai>
2024-07-17 21:48:34 +0530

    78. Subsets

--- Leetcode/Medium/78. Subsets.cpp
+++ Leetcode/Medium/78. Subsets.cpp
@@ -8,34 +8,38 @@ using namespace std;
 
 //! Using Bit Manipulation
 
-class Solution
-{
+// # Tutorial: https://youtube.com/watch?v=LqKaUv1G3_I&list=PLgUwDviBIf0rnqh8QsJaHyIX7KUiaPUv7&index=43
+
+/*
+> Time Complexity: O(2^N * N)
+> Space Complexity: ~ O(2^N * N) => nearabout not exact
+*/
+
+class Solution {
 public:
- vector<vector<int>> subsets(vector<int> &nums)
- {
-  int n = nums.size();
-  vector<vector<int>> ans;
-  //* generate all numbers from 0 -> 2^n - 1
-  for (int num = 0; num < (1 << n); num++)
-  {
-   vector<int> subset;
-   for (int i = 0; i < n; i++)
-   {
-    //* check if it's a set bit
-    if ((num & (1 << i)) != 0)
-    {
-     subset.push_back(nums[i]);
+    vector<vector<int>> subsets(vector<int>& nums) {
+        //* generate all numbers from 0->2^n-1, and check their last n bits where n = nums.size(), if ith bit is 1 we pick the ith number from nums in our subset else skip it
+
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for(int num = 0; num < (1 << n); num++) {
+            vector<int> subset;
+            //* check if ith bit is set bit
+            for(int i = 0; i < n; i++) {
+                if((num & (1 << i)) != 0) {
+                    subset.push_back(nums[i]);
+                }
+            }
+            ans.push_back(subset);
+        }
+        return ans;
     }
-   }
-   ans.push_back(subset);
-  }
-  return ans;
- }
 };
 
-// # Tutorial: https://www.youtube.com/watch?v=AxNNVECce8c&list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9&index=8
 
 //! Using Backtracking (Pick & Not Pick)
+
+// # Tutorial: https://www.youtube.com/watch?v=AxNNVECce8c&list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9&index=8
 
 /*
 > Time Complexity: O(2^N) => for every index we have 2 options
@@ -45,29 +49,29 @@ public:
 class Solution
 {
 public:
- vector<vector<int>> helper(vector<int> &nums, vector<vector<int>> &ans, vector<int> subSet, int index, int n)
- {
-  if (index == n)
-  {
-   ans.push_back(subSet);
-   return ans;
-  }
+    vector<vector<int>> helper(vector<int> &nums, vector<vector<int>> &ans, vector<int> subSet, int index, int n)
+    {
+        if (index == n)
+        {
+            ans.push_back(subSet);
+            return ans;
+        }
 
-  //> pick the particular index into the subsequence
-  subSet.push_back(nums[index]);
-  helper(nums, ans, subSet, index + 1, n);
+        //> pick the particular index into the subsequence
+        subSet.push_back(nums[index]);
+        helper(nums, ans, subSet, index + 1, n);
 
-  //> not pick the particular index into the subsequence
-  subSet.pop_back();
-  helper(nums, ans, subSet, index + 1, n);
+        //> not pick the particular index into the subsequence
+        subSet.pop_back();
+        helper(nums, ans, subSet, index + 1, n);
 
-  return ans;
- }
+        return ans;
+    }
 
- vector<vector<int>> subsets(vector<int> &nums)
- {
-  vector<vector<int>> ans;
-  vector<int> subSet;
-  return helper(nums, ans, subSet, 0, nums.size());
- }
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        vector<int> subSet;
+        return helper(nums, ans, subSet, 0, nums.size());
+    }
 };
