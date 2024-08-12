14647a2c4739a3d89c7ef48f4e8152fcaec534bf
Kumar Harsh <harsh@rubick.ai>
2024-08-13 01:27:12 +0530

    225. Implement Stack using Queues

--- Leetcode/Easy/225. Implement Stack using Queues.cpp
+++ Leetcode/Easy/225. Implement Stack using Queues.cpp
@@ -1,48 +1,54 @@
 #include <bits/stdc++.h>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
 #include <cstring>
 using namespace std;
 
+// # Tutorial: https://www.youtube.com/watch?v=tqQ5fTamIN4&list=PLgUwDviBIf0pOd5zvVVSzgpo6BaCpHT9c&index=1
+
 //! Using 1 Queue
 
 class MyStack
 {
 public:
- queue<int> q1;
+    queue<int> q;
 
- MyStack()
- {
- }
+    MyStack()
+    {
+    }
 
- void push(int x)
- {
-  q1.push(x);
-  for (int i = 0; i < q1.size() - 1; i++)
-  {
-   q1.push(q1.front());
-   q1.pop();
-  }
- }
+    /* TC: O(N) */
+    //* every time we push a new element we have to reverse the stack so that the current element is always at front of queue
+    void push(int x)
+    {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+    }
 
- int pop()
- {
-  int top_element = q1.front();
-  q1.pop();
-  return top_element;
- }
+    /* TC: O(1) */
+    int pop()
+    {
+        int element = q.front();
+        q.pop();
+        return element;
+    }
 
- int top()
- {
-  return q1.front();
- }
+    int top()
+    {
+        return q.front();
+    }
 
- bool empty()
- {
-  return q1.empty();
- }
+    bool empty()
+    {
+        return q.size() == 0;
+    }
 };
 
 //! Using 2 Queues
@@ -50,46 +56,37 @@ public:
 class MyStack
 {
 public:
- queue<int> q1, q2;
+    queue<int> q1, q2;
 
- MyStack()
- {
- }
+    MyStack()
+    {
+    }
 
- void push(int x)
- {
-  q2.push(x);
-  while (!q1.empty())
-  {
-   q2.push(q1.front());
-   q1.pop();
-  }
-  swap(q1, q2);
- }
+    void push(int x)
+    {
+        q2.push(x);
+        while (!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1, q2);
+    }
 
- int pop()
- {
-  int top_element = q1.front();
-  q1.pop();
-  return top_element;
- }
+    int pop()
+    {
+        int top_element = q1.front();
+        q1.pop();
+        return top_element;
+    }
 
- int top()
- {
-  return q1.front();
- }
+    int top()
+    {
+        return q1.front();
+    }
 
- bool empty()
- {
-  return q1.empty();
- }
+    bool empty()
+    {
+        return q1.empty();
+    }
 };
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
