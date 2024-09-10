e1540b4e2439711c6c7d50434e97f1701d9d25f3
Kumar Harsh <harsh@rubick.ai>
2024-09-10 22:10:10 +0530

    807. Insert Greatest Common Divisors in Linked List

---
+++ Leetcode/Medium/807. Insert Greatest Common Divisors in Linked List.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *insertGreatestCommonDivisors(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+        {
+            return head;
+        }
+
+        ListNode *node1 = head;
+        ListNode *node2 = head->next;
+
+        while (node2 != nullptr)
+        {
+            int gcdValue = calculateGCD(node1->val, node2->val);
+            ListNode *gcdNode = new ListNode(gcdValue);
+
+            // Insert gcdNode between node1 and node2
+            node1->next = gcdNode;
+            gcdNode->next = node2;
+
+            // Move to the next pair of nodes
+            node1 = node2;
+            node2 = node2->next;
+        }
+
+        return head;
+    }
+
+private:
+    //* The core idea of the Euclidean algorithm is based on the principle that the GCD of two numbers a and b (where a > b) is the same as the GCD of b and a % b (the remainder when a is divided by b)
+    int calculateGCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+};
