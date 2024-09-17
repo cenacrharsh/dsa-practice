74a836fdf56893fae64b8edfc4c1cda27fa77888
Kumar Harsh <harsh@rubick.ai>
2024-09-18 01:23:16 +0530

    884. Uncommon Words from Two Sentences

--- Leetcode/Easy/884. Uncommon Words from Two Sentences.cpp
+++ Leetcode/Easy/884. Uncommon Words from Two Sentences.cpp
@@ -9,53 +9,45 @@ using namespace std;
 class Solution
 {
 public:
- vector<string> uncommonFromSentences(string s1, string s2)
- {
-  unordered_map<string, int> freq1;
-  unordered_map<string, int> freq2;
-  string word = "";
-  for (int i = 0; i < s1.size(); i++)
-  {
-   if (s1[i] == ' ')
-   {
-    freq1[word]++;
-    word = "";
-   }
-   else
-   {
-    word += s1[i];
-   }
-  }
-  freq1[word]++;
-  word = "";
-  for (int i = 0; i < s2.size(); i++)
-  {
-   if (s2[i] == ' ')
-   {
-    freq2[word]++;
-    word = "";
-   }
-   else
-   {
-    word += s2[i];
-   }
-  }
-  freq2[word]++;
-  vector<string> ans;
-  for (auto i : freq1)
-  {
-   if (i.second == 1 && freq2.find(i.first) == freq2.end())
-   {
-    ans.push_back(i.first);
-   }
-  }
-  for (auto i : freq2)
-  {
-   if (i.second == 1 && freq1.find(i.first) == freq1.end())
-   {
-    ans.push_back(i.first);
-   }
-  }
-  return ans;
- }
+    vector<string> uncommonFromSentences(string s1, string s2)
+    {
+        unordered_map<string, int> freq;
+        string word = "";
+        s1 += " ";
+        for (int i = 0; i < s1.size(); i++)
+        {
+            if (s1[i] != ' ')
+            {
+                word += s1[i];
+            }
+            else
+            {
+                freq[word]++;
+                word = "";
+            }
+        }
+        word = "";
+        s2 += " ";
+        for (int i = 0; i < s2.size(); i++)
+        {
+            if (s2[i] != ' ')
+            {
+                word += s2[i];
+            }
+            else
+            {
+                freq[word]++;
+                word = "";
+            }
+        }
+        vector<string> ans;
+        for (auto it : freq)
+        {
+            if (it.second == 1)
+            {
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
 };
