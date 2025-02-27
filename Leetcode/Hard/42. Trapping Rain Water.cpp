f96fbff35f6eb9bb20037e2ee8e8f0a6989842b1
Kumar Harsh <harsh@rubick.ai>
2025-02-28 01:02:17 +0530

    42

--- Leetcode/Hard/42. Trapping Rain Water.cpp
+++ Leetcode/Hard/42. Trapping Rain Water.cpp
@@ -4,42 +4,45 @@
 #include <climits>
 using namespace std;
 
+/*
+- For every building we can only store water if there is a taller building to it's left and right.
+- Total Water = Summation (0->n-1) min(leftMax, rightMax) - arr[i], and the main challenge is to find the leftMax and rightMax for each building in optimal time.
+*/
+
 class Solution
 {
 public:
- int trap(vector<int> &height)
- {
-  int leftMax = INT_MIN;
-  int rightMax = INT_MIN;
-  int water = 0;
-  int l = 0, r = height.size() - 1;
-  while (l <= r)
+  int trap(vector<int> &height)
   {
-   if (height[l] <= height[r])
-   {
-    if (height[l] >= leftMax)
+    int leftMax = 0, rightMax = 0, left = 0, right = height.size() - 1, totalWater = 0;
+    while (left <= right)
     {
-     leftMax = height[l];
+      //* since we always deal with whichever one on either left or right is smaller we are always sure of the fact that if we are currently operating on left it's assured that a height >= currHeight exists in right
+      if (height[left] <= height[right])
+      {
+        if (leftMax > height[left])
+        {
+          totalWater += (leftMax - height[left]);
+        }
+        else
+        {
+          leftMax = height[left];
+        }
+        left++;
+      }
+      else
+      {
+        if (rightMax > height[right])
+        {
+          totalWater += (rightMax - height[right]);
+        }
+        else
+        {
+          rightMax = height[right];
+        }
+        right--;
+      }
     }
-    else
-    {
-     water += leftMax - height[l];
-    }
-    l++;
-   }
-   else if (height[r] < height[l])
-   {
-    if (height[r] >= rightMax)
-    {
-     rightMax = height[r];
-    }
-    else
-    {
-     water += rightMax - height[r];
-    }
-    r--;
-   }
+    return totalWater;
   }
-  return water;
- }
 };
