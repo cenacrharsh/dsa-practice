3a9a36c30893685e730f139405a708b15bb94b0f
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2025-12-16 00:56:16 +0530

    673. Number of Longest Increasing Subsequence

--- Leetcode/Medium/673. Number of Longest Increasing Subsequence.cpp
+++ Leetcode/Medium/673. Number of Longest Increasing Subsequence.cpp
@@ -1,5 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
@@ -8,39 +10,76 @@ using namespace std;
 
 // # Tutorial: https://www.youtube.com/watch?v=cKVl1TFdNXg&list=PLgUwDviBIf0pwFf-BnpkXxs0Ra0eU2sJY&index=25
 
+//! Space Optimized Tabulation DP
+
+/*
+> Time Complexity: O()
+> Space Complexity: O()
+*/
+
+//! Tabulation DP -> Bottom Up Approach
+
+/*
+> Time Complexity: O(N^2) + O(N)
+> Space Complexity: O(N)
+*/
+
 class Solution
 {
 public:
     int findNumberOfLIS(vector<int> &nums)
     {
+        //* we'll maintain a count vector along with dp vector, dp maintains the longest inc subs ending at index i but in count vector we'll maintain how many such subs we have ending at index i, so whenever a new count comes in dp array we'll inherit the initial count of that new subs but as the same length subs keep appearing we'll keep increasing the count for that particular length subs
+
         int n = nums.size();
         vector<int> dp(n, 1), count(n, 1);
-        int lisLen = 1;
-        for (int curr = 1; curr < n; curr++)
-        {
-            for (int prev = 0; prev < curr; prev++)
-            {
-                if (nums[prev] < nums[curr] && 1 + dp[prev] > dp[curr])
-                {
-                    dp[curr] = 1 + dp[prev];
-                    count[curr] = count[prev]; //* inherit
-                }
-                else if (nums[prev] < nums[curr] && 1 + dp[prev] == dp[curr])
-                {
-                    count[curr] += count[prev]; //* inc the count
-                }
-            }
-            lisLen = max(lisLen, dp[curr]);
-        }
+        int maxLen = 1;
 
-        int noOfLIS = 0;
         for (int i = 0; i < n; i++)
         {
-            if (dp[i] == lisLen)
+            for (int prev = 0; prev < i; prev++)
             {
-                noOfLIS += count[i];
+                if (nums[prev] < nums[i] && 1 + dp[prev] > dp[i])
+                {
+                    dp[i] = 1 + dp[prev];
+                    //* inherit the count of the this new length subs
+                    count[i] = count[prev];
+                }
+                else if (nums[prev] < nums[i] && 1 + dp[prev] == dp[i])
+                {
+                    //* we encounter the same length subs so we inc the count
+                    count[i] += count[prev];
+                }
+            }
+            if (dp[i] > maxLen)
+            {
+                maxLen = dp[i];
             }
         }
-        return noOfLIS;
+
+        //* we'll calculate the sum total of of max length subs
+        int noOfLis = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (dp[i] == maxLen)
+            {
+                noOfLis += count[i];
+            }
+        }
+        return noOfLis;
     }
 };
+
+//! Memoization DP
+
+/*
+> Time Complexity: O()
+> Space Complexity: O()
+*/
+
+//! Recursion -> Top Down Approach
+
+/*
+> Time Complexity: O()
+> Space Complexity: O()
+*/
