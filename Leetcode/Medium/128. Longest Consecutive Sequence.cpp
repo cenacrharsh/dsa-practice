8d8c427c4739c0574e7e4820865c9910b0134860
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-02-16 22:52:23 +0530

    128. Longest Consecutive Sequence

--- Leetcode/Medium/128. Longest Consecutive Sequence.cpp
+++ Leetcode/Medium/128. Longest Consecutive Sequence.cpp
@@ -1,39 +1,98 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
 #include <cstring>
+#include <unordered_set>
 using namespace std;
 
-// # Tutorial: https://www.youtube.com/watch?v=qgizvmgeyUM
+//! Brute Force (Loop and use Linear Search)
 
 /*
-> Time Complexity: O(n) + O(n) + O(n)
-> Space Complexity: O(n)
+> Time Complexity: O(N^3)
+> Space Complexity: O(1)
+*/
+
+//! Sorting (Better)
+
+/*
+> Time Complexity: O(N * Log N)
+> Space Complexity: O(1)
 */
 
 class Solution
 {
 public:
- int longestConsecutive(vector<int> &nums)
- {
-  unordered_set<int> seq(nums.begin(), nums.end());
-  int maxLen = 0;
-  for (auto num : nums)
-  {
-   if (seq.find(num - 1) == seq.end())
-   {
-    int len = 0;
-    int currNum = num;
-    while (seq.find(currNum) != seq.end())
+    int longestConsecutive(vector<int> &nums)
     {
-     len++;
-     currNum++;
+        sort(nums.begin(), nums.end());
+
+        int longestConsecutiveLength = 0, currCount = 0, lastSmaller = INT_MIN;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] - 1 == lastSmaller)
+            {
+                currCount++;
+                lastSmaller = nums[i];
+            }
+            else if (nums[i] != lastSmaller)
+            {
+                //* we don't have to do anything if last element is same as current element
+                currCount = 1;
+                lastSmaller = nums[i];
+            }
+
+            longestConsecutiveLength = max(longestConsecutiveLength, currCount);
+        }
+        return longestConsecutiveLength;
     }
-    maxLen = max(maxLen, len);
-   }
-  }
-  return maxLen;
- }
+};
+
+//! Unordered Set (Optimal)
+
+/*
+> Time Complexity: O(N) + O(2N) ~ Internal while loop takes around 2N not N^2 as we only start from head of seq
+> Space Complexity: O(N)
+*/
+
+class Solution
+{
+public:
+    int longestConsecutive(vector<int> &nums)
+    {
+        if (nums.size() == 0)
+        {
+            return 0;
+        }
+
+        // unordered_set<int> st(nums.begin(), nums.end());
+        unordered_set<int> st;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            st.insert(nums[i]);
+        }
+
+        int longestConsecutiveLength = 1;
+        //* iterate over the set instead of array to avoid duplicates
+        for (auto num : st)
+        {
+            //* always check if n-1 exists to avoid calculations mid sequence, always start at the head of the sequence
+            if (st.find(num - 1) == st.end())
+            {
+                int currCount = 1;
+                int x = num;
+                while (st.find(x + 1) != st.end())
+                {
+                    x++;
+                    currCount++;
+                }
+                longestConsecutiveLength = max(longestConsecutiveLength, currCount);
+            }
+        }
+        return longestConsecutiveLength;
+    }
 };
--- Leetcode/Medium/912. Sort an Array.cpp
+++ Leetcode/Medium/912. Sort an Array.cpp
@@ -1,127 +1,194 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
+#include <cstring>
 using namespace std;
 
 //! Merge Sort
 
 /*
-> Time Complexity: O(n * log n)
-> Space Complexity: O(n)
+> Time Complexity: O(N * Log N)
+> Space Complexity: O(N)
 */
 
 class Solution
 {
 public:
- void merge(vector<int> &nums, int l, int mid, int r)
- {
-  int n1 = mid - l + 1;
-  int n2 = r - mid;
-  vector<int> left(n1);
-  vector<int> right(n2);
-  int i, j, k;
-  for (i = 0; i < n1; i++)
-  {
-   left[i] = nums[l + i];
-  }
-  for (j = 0; j < n2; j++)
-  {
-   right[j] = nums[mid + 1 + j];
-  }
-  i = 0;
-  j = 0;
-  k = l;
-  while (i < n1 && j < n2)
-  {
-   if (left[i] < right[j])
-   {
-    nums[k++] = left[i++];
-   }
-   else
-   {
-    nums[k++] = right[j++];
-   }
-  }
-  while (i < n1)
-  {
-   nums[k++] = left[i++];
-  }
-  while (j < n2)
-  {
-   nums[k++] = right[j++];
-  }
- }
- void mergeSort(vector<int> &nums, int l, int r)
- {
-  if (l < r)
-  {
-   int mid = (l + r) / 2;
+    void merge(vector<int> &nums, int low, int mid, int high)
+    {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
 
-   mergeSort(nums, l, mid);
-   mergeSort(nums, mid + 1, r);
+        while (left <= mid && right <= high)
+        {
+            if (nums[left] <= nums[right])
+            {
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else
+            {
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
 
-   merge(nums, l, mid, r);
-  }
- }
- vector<int> sortArray(vector<int> &nums)
- {
-  mergeSort(nums, 0, nums.size() - 1);
-  return nums;
- }
+        while (left <= mid)
+        {
+            temp.push_back(nums[left]);
+            left++;
+        }
+
+        while (right <= high)
+        {
+            temp.push_back(nums[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++)
+        {
+            nums[i] = temp[i - low];
+        }
+    }
+
+    void mergeSort(vector<int> &nums, int low, int high)
+    {
+        if (low >= high)
+        {
+            return;
+        }
+
+        int mid = (low + high) / 2;
+
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid + 1, high);
+        merge(nums, low, mid, high);
+    }
+
+    vector<int> sortArray(vector<int> &nums)
+    {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
+
+//! Quick Sort
+
+/*
+> Time Complexity: O(N * Log N) ~ Worst O(N^2)
+> Space Complexity: O(N)
+*/
+
+class Solution
+{
+public:
+    int partition(vector<int> &nums, int low, int high)
+    {
+        int pivot = nums[low]; //* assume the first element as pivot element
+
+        int i = low;
+        int j = high;
+
+        while (i < j)
+        {
+            //* find first element greater than pivot on the left, use high - 1 so that after i++ we are still within bounds
+            while (nums[i] <= pivot && i <= high - 1)
+            {
+                i++;
+            }
+
+            //* find the first element lower than pivot on the right, use low + 1 so that after i++ we are still within bounds
+            while (nums[j] > pivot && j >= low + 1)
+            {
+                j--;
+            }
+
+            if (i < j)
+            {
+                swap(nums[i], nums[j]);
+            }
+        }
+
+        //* swap the pivot with last element of left half
+        swap(nums[low], nums[j]);
+        return j;
+    }
+
+    void quickSort(vector<int> &nums, int low, int high)
+    {
+        if (low >= high)
+        {
+            return;
+        }
+
+        int partitionIndex = partition(nums, low, high);
+
+        quickSort(nums, 0, partitionIndex - 1);
+        quickSort(nums, partitionIndex + 1, high);
+    }
+
+    vector<int> sortArray(vector<int> &nums)
+    {
+        quickSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
 };
 
 //! Randomized Quick Sort
 
 /*
-> Time Complexity: O(n * log n) -> O(n2) in worst cases
+> Time Complexity: O(N * Log N) -> O(N^2) in worst cases
 > Space Complexity: O(1)
 */
 
 class Solution
 {
 public:
- int randomPartition(vector<int> &nums, int l, int r)
- {
-  // select random element of array :
-  srand(time(NULL));
-  int pivot = l + rand() % (r - l);
-  swap(nums[pivot], nums[r]);
-  return partition(nums, l, r);
- }
+    int randomPartition(vector<int> &nums, int l, int r)
+    {
+        // select random element of array :
+        srand(time(NULL));
+        int pivot = l + rand() % (r - l);
+        swap(nums[pivot], nums[r]);
+        return partition(nums, l, r);
+    }
 
- int partition(vector<int> &nums, int l, int r)
- {
-  int pivot = nums[r];
+    int partition(vector<int> &nums, int l, int r)
+    {
+        int pivot = nums[r];
 
-  int j = l;
+        int j = l;
 
-  for (int i = l; i <= r; i++)
-  {
-   if (nums[i] <= pivot)
-   {
-    swap(nums[i], nums[j]);
-    j++;
-   }
-  }
-  return j - 1;
- }
+        for (int i = l; i <= r; i++)
+        {
+            if (nums[i] <= pivot)
+            {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+        return j - 1;
+    }
 
- void quickSort(vector<int> &nums, int l, int r)
- {
-  if (l < r)
-  {
-   int pivot_index = randomPartition(nums, l, r);
+    void quickSort(vector<int> &nums, int l, int r)
+    {
+        if (l < r)
+        {
+            int pivot_index = randomPartition(nums, l, r);
 
-   quickSort(nums, l, pivot_index - 1);
-   quickSort(nums, pivot_index + 1, r);
-  }
- }
+            quickSort(nums, l, pivot_index - 1);
+            quickSort(nums, pivot_index + 1, r);
+        }
+    }
 
- vector<int> sortArray(vector<int> &nums)
- {
-  quickSort(nums, 0, nums.size() - 1);
-  return nums;
- }
+    vector<int> sortArray(vector<int> &nums)
+    {
+        quickSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
 };
