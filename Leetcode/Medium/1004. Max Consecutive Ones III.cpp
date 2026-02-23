2cc467db0aa5cbf745c1ea8846f53c6b5e6a7c30
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-02-23 22:29:35 +0530

    1004. Max Consecutive Ones III

--- Leetcode/Medium/1004. Max Consecutive Ones III.cpp
+++ Leetcode/Medium/1004. Max Consecutive Ones III.cpp
@@ -1,12 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
 #include <cstring>
 using namespace std;
-
-//# Tutorial: https://www.youtube.com/watch?v=3E4JBHSLpYk&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=9
 
 //! Optimized Sliding Window
 
@@ -15,31 +15,38 @@ using namespace std;
 > Space Complexity: O(1)
 */
 
-class Solution {
+class Solution
+{
 public:
-    int longestOnes(vector<int>& nums, int k) {
-        //* longest subarray with at most k zeroes
-        //* optimized sliding window we don't let window size drop below the max size we have found already as that's our ans if we don't find anything bigger than that so no point in shrinking window and then increasing it again, thus removing the inner while loop
+    int longestOnes(vector<int> &nums, int k)
+    {
+        //* optimized sliding window we don't let window size drop below the max size we have found already as that's our ans if we don't find anything bigger than that so no point in shrinking window below this size and then increasing it again, thus removing the inner while loop
 
-        int left = 0, right = 0, numZeroes = 0, maxLen = INT_MIN;
-        while(right < nums.size()) {
-            if(nums[right] == 0) {
-                numZeroes++;
+        int left = 0, right = 0, maxCount = 0, zeroCount = 0;
+        while (right < nums.size())
+        {
+            if (nums[right] == 0)
+            {
+                zeroCount++;
             }
 
-            if(numZeroes > k) {
-                if(nums[left] == 0) {
-                    numZeroes--;
+            if (zeroCount > k)
+            {
+                if (nums[left] == 0)
+                {
+                    zeroCount--;
                 }
                 left++;
             }
 
-            if(numZeroes <= k) {
-                maxLen = max(maxLen, right - left + 1);
+            if (zeroCount <= k)
+            {
+                maxCount = max(maxCount, right - left + 1);
             }
+
             right++;
         }
-        return maxLen;
+        return maxCount;
     }
 };
 
@@ -50,27 +57,72 @@ public:
 > Space Complexity: O(1)
 */
 
-class Solution {
+class Solution
+{
 public:
-    int longestOnes(vector<int>& nums, int k) {
-        //* longest subarray with at most k zeroes
-        int left = 0, right = 0, numZeroes = 0, maxLen = INT_MIN;
-        while(right < nums.size()) {
-            if(nums[right] == 0) {
-                numZeroes++;
+    int longestOnes(vector<int> &nums, int k)
+    {
+        int left = 0, right = 0, maxCount = 0, zeroCount = 0;
+        while (right < nums.size())
+        {
+            if (nums[right] == 0)
+            {
+                zeroCount++;
             }
 
-            while(numZeroes > k) {
-                if(nums[left] == 0) {
-                    numZeroes--;
+            while (zeroCount > k)
+            {
+                if (nums[left] == 0)
+                {
+                    zeroCount--;
                 }
                 left++;
             }
-            
-            maxLen = max(maxLen, right - left + 1);
+
+            if (zeroCount <= k)
+            {
+                maxCount = max(maxCount, right - left + 1);
+            }
+
             right++;
         }
-        return maxLen;
+        return maxCount;
     }
 };
 
+//! Brute Force
+
+/*
+> Time Complexity: O(N^2)
+> Space Complexity: O(1)
+*/
+
+class Solution
+{
+public:
+    int longestOnes(vector<int> &nums, int k)
+    {
+        int maxCount = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int zeroCount = 0;
+            for (int j = i; j < nums.size(); j++)
+            {
+                if (nums[j] == 0)
+                {
+                    zeroCount++;
+                }
+
+                if (zeroCount <= k)
+                {
+                    maxCount = max(maxCount, j - i + 1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return maxCount;
+    }
+};
