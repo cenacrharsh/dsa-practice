b2337bdf1f539a418c79df05a313ac7c02bb5da4
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-02-23 21:28:11 +0530

    3. Longest Substring Without Repeating Characters

--- Leetcode/Medium/3. Longest Substring Without Repeating Characters.cpp
+++ Leetcode/Medium/3. Longest Substring Without Repeating Characters.cpp
@@ -1,27 +1,63 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
 #include <cstring>
 using namespace std;
 
-// # Tutorial: https://www.youtube.com/watch?v=-zSxTJkcdAo&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=3
-
-//! Using Freq Array
+//! Optimal, 2 Pointer + Sliding Window + Using Freq Array
 
 /*
-> Time Complexity: O(N)
+> Time Complexity: O()
+> Space Complexity: O()
+*/
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int left = 0, right = 0, maxLen = 0;
+        vector<int> freq(256, -1);
+        while (right < s.size())
+        {
+            if (freq[s[right]] != -1)
+            {
+                //* even if element present in hashmap if it's out of the left->right substring it doesn't matter
+                if (freq[s[right]] >= left)
+                {
+                    left = freq[s[right]] + 1; //* if current char is found at index freq[s[right]] we go to one index ahead
+                }
+            }
+
+            freq[s[right]] = right;
+            maxLen = max(maxLen, right - left + 1);
+            right++;
+        }
+        return maxLen;
+    }
+};
+
+/*
+> Time Complexity: ~ O(2N)
 > Space Complexity: O(256)
 */
 
-class Solution {
+class Solution
+{
 public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> freq(256, 0);
-        int left = 0, right = 0, maxLen = 0;
-        while(right < s.size()) {
-            while(freq[s[right]] != 0) {
+    int lengthOfLongestSubstring(string s)
+    {
+        int left = 0, right = 0;
+        int maxLen = 0;
+        vector<int> freq(256); //* there are 256 different characters
+        while (right < s.size())
+        {
+            while (freq[s[right]] != 0)
+            {
                 freq[s[left]]--;
                 left++;
             }
@@ -33,29 +69,62 @@ public:
     }
 };
 
-//! Using Hashmap
+//! Optimal, 2 Pointer + Slidng Window + Using Hashmap
 
 /*
-> Time Complexity: O(N)
+> Time Complexity: ~ O(2N)
 > Space Complexity: O(1)
 */
 
-class Solution {
+class Solution
+{
 public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_map<int, int> freq; //* char->index
-        int left = 0, right = 0, maxLen = 0;
-        while(right < s.size()) {
-            while(freq.find(s[right]) != freq.end()) {
-                freq[s[left]]--;
-                if(freq[s[left]] == 0) {
-                    freq.erase(s[left]);
-                }
+    int lengthOfLongestSubstring(string s)
+    {
+        int left = 0, right = 0;
+        int maxLen = 0;
+        unordered_map<char, int> mp;
+        while (right < s.size())
+        {
+            while (mp[s[right]] != 0)
+            {
+                mp[s[left]]--;
                 left++;
             }
             maxLen = max(maxLen, right - left + 1);
-            freq[s[right]]++;
+            mp[s[right]]++;
             right++;
+        }
+        return maxLen;
+    }
+};
+
+//! Brute Force, Generate all substrings
+
+/*
+> Time Complexity: O(N^2)
+> Space Complexity: O(256)
+*/
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int maxLen = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            vector<int> freq(256, 0);
+            for (int j = i; j < s.size(); j++)
+            {
+                if (freq[s[j]] == 1)
+                {
+                    break;
+                }
+
+                freq[s[j]] = 1;
+                maxLen = max(maxLen, j - i + 1);
+            }
         }
         return maxLen;
     }
