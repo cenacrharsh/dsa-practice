a9d7c789e935a02a93b3be5fb738d4fe505bb810
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-02-24 22:28:46 +0530

    904. Fruit Into Baskets

--- Leetcode/Medium/904. Fruit Into Baskets.cpp
+++ Leetcode/Medium/904. Fruit Into Baskets.cpp
@@ -1,87 +1,94 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
 #include <cstring>
 using namespace std;
 
-//! Solution 1
+//! Optimal, 2 Pointers + Optimized Sliding Window
+
+/*
+> Time Complexity: O(N) + O(1)
+> Space Complexity: O(2)
+*/
 
 class Solution
 {
 public:
- int totalFruit(vector<int> &fruits)
- {
-  int count = 0;
-  int maxCount = INT_MIN;
-  int l = 0, r = 0;
-  unordered_map<int, int> check;
-  while (r < fruits.size())
-  {
-   if (check.find(fruits[r]) != check.end())
-   {
-    count++;
-    maxCount = max(maxCount, count);
-    check[fruits[r]]++;
-    r++;
-   }
-   else
-   {
-    if (check.size() < 2)
+    int totalFruit(vector<int> &fruits)
     {
-     count++;
-     maxCount = max(maxCount, count);
-     check[fruits[r]]++;
-     r++;
+        //* boils down to longest substring with at most 2 different characters
+        unordered_map<int, int> mp;
+        int maxLength = 0, left = 0, right = 0;
+        while (right < fruits.size())
+        {
+            mp[fruits[right]]++;
+
+            if (mp.size() > 2)
+            {
+                mp[fruits[left]]--;
+                if (mp[fruits[left]] == 0)
+                {
+                    mp.erase(fruits[left]);
+                }
+                left++;
+            }
+
+            if (mp.size() <= 2)
+            {
+                maxLength = max(maxLength, right - left + 1);
+            }
+            right++;
+        }
+        return maxLength;
     }
-    else
-    {
-     if (check[fruits[l]] == 1)
-     {
-      check.erase(fruits[l]);
-     }
-     else
-     {
-      check[fruits[l]]--;
-     }
-     l++;
-     count--;
-    }
-   }
-  }
-  return maxCount;
- }
 };
 
-//! Solution 2
+//! Better, 2 Pointers + Sliding Window
+
+/*
+> Time Complexity: O(N) + O(N) + O(1), O(Log 256) if ordered map
+> Space Complexity: O(2)
+*/
 
 class Solution
 {
 public:
- int totalFruit(vector<int> &fruits)
- {
-  int count = 0;
-  int maxCount = INT_MIN;
-  int l = 0, r = 0;
-  unordered_map<int, int> check;
-  while (r < fruits.size())
-  {
-   check[fruits[r]]++;
-   while (check.size() > 2)
-   {
-    check[fruits[l]]--;
-    if (check[fruits[l]] == 0)
+    int totalFruit(vector<int> &fruits)
     {
-     check.erase(fruits[l]);
+        //* boils down to longest substring with at most 2 different characters
+        unordered_map<int, int> mp;
+        int maxLength = 0, left = 0, right = 0;
+        while (right < fruits.size())
+        {
+            mp[fruits[right]]++;
+
+            while (mp.size() > 2)
+            {
+                mp[fruits[left]]--;
+                if (mp[fruits[left]] == 0)
+                {
+                    mp.erase(fruits[left]);
+                }
+                left++;
+            }
+
+            if (mp.size() <= 2)
+            {
+                maxLength = max(maxLength, right - left + 1);
+            }
+            right++;
+        }
+        return maxLength;
     }
-    l++;
-    count--;
-   }
-   count++;
-   maxCount = max(maxCount, count);
-   r++;
-  }
-  return maxCount;
- }
 };
+
+//! Brute Force, Generate all substrings
+
+/*
+> Time Complexity: O(N^2) * O(Log 256) if map, O(1) if unordered_map
+> Space Complexity: O(2)
+*/
