ca9e8a26004da58af0469ca763bc5246b428b80f
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-02-25 23:06:48 +0530

    1423. Maximum Points You Can Obtain from Cards

--- Leetcode/Medium/1423. Maximum Points You Can Obtain from Cards.cpp
+++ Leetcode/Medium/1423. Maximum Points You Can Obtain from Cards.cpp
@@ -1,36 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
 #include <cstring>
 using namespace std;
 
-// # Tutorial: https://www.youtube.com/watch?v=pBWCOCS636U&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=2
-
 //! Sliding Window
 
 /*
-> Time Complexity: O(2K)
+> Time Complexity: O(2 * K)
 > Space Complexity: O(1)
 */
 
-class Solution {
+class Solution
+{
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        int cardSum = 0, maxCardSum = 0;
-        for(int i = 0; i < k; i++) {
-            cardSum += cardPoints[i];
+    int maxScore(vector<int> &cardPoints, int k)
+    {
+        int leftSum = 0, rightSum = 0, maxSum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            leftSum += cardPoints[i];
         }
-        maxCardSum = max(maxCardSum, cardSum);
-        int left = k - 1, right = cardPoints.size() - 1;
-        while(left >= 0) {
-            cardSum -= cardPoints[left];
-            left--;
-            cardSum += cardPoints[right];
-            right--;
-            maxCardSum = max(maxCardSum, cardSum);
+        maxSum = max(maxSum, leftSum);
+        int rightIndex = cardPoints.size() - 1;
+        for (int i = k - 1; i >= 0; i--)
+        {
+            leftSum -= cardPoints[i];
+            rightSum += cardPoints[rightIndex];
+            rightIndex--;
+            maxSum = max(maxSum, leftSum + rightSum);
         }
-        return maxCardSum;
+        return maxSum;
     }
 };
