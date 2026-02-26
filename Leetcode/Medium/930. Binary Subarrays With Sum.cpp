b8820893e36a610e5df9b8f690c07c64d3552dba
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-02-26 22:53:27 +0530

    moved to legacy folder

--- Leetcode/Medium/930. Binary Subarrays With Sum.cpp
+++ Leetcode/Medium/930. Binary Subarrays With Sum.cpp
@@ -1,12 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
 #include <cstring>
 using namespace std;
-
-// # Tutorial: https://www.youtube.com/watch?v=XnMdNUkX6VM&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=9
 
 //! Sliding Window
 
@@ -15,17 +15,22 @@ using namespace std;
 > Space Complexity: O(1)
 */
 
-class Solution {
+class Solution
+{
 public:
-    int helper(vector<int>& nums, int goal) {
-        if(goal < 0) {
+    int helper(vector<int> &nums, int goal)
+    {
+        if (goal < 0)
+        {
             return 0;
         }
         int left = 0, right = 0, count = 0, sum = 0;
-        while(right < nums.size()) {
+        while (right < nums.size())
+        {
             sum += nums[right];
 
-            while(sum > goal) {
+            while (sum > goal)
+            {
                 sum -= nums[left];
                 left++;
             }
@@ -36,7 +41,8 @@ public:
         return count;
     }
 
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int numSubarraysWithSum(vector<int> &nums, int goal)
+    {
         //* sum(k) == sum(<=k) - sum(<=k-1)
         return helper(nums, goal) - helper(nums, goal - 1);
     }
@@ -49,17 +55,49 @@ public:
 > Space Complexity: O(N)
 */
 
-class Solution {
+class Solution
+{
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int numSubarraysWithSum(vector<int> &nums, int goal)
+    {
         int count = 0;
         int prefixSum = 0;
         unordered_map<int, int> prefixSumMap;
         prefixSumMap[prefixSum] = 1;
-        for(int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
+        {
             prefixSum += nums[i];
             count += prefixSumMap[prefixSum - goal];
             prefixSumMap[prefixSum]++;
+        }
+        return count;
+    }
+};
+
+//! Brute Force
+
+/*
+> Time Complexity: O(N^2)
+> Space Complexity: O(1)
+*/
+
+class Solution
+{
+public:
+    int numSubarraysWithSum(vector<int> &nums, int goal)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int tempSum = 0;
+            for (int j = i; j < nums.size(); j++)
+            {
+                tempSum += nums[j];
+                if (tempSum == goal)
+                {
+                    count++;
+                }
+            }
         }
         return count;
     }
