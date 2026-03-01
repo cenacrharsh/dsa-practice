99676a64d03fb22a934abd31e848f164280b22a9
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-03-01 17:27:26 +0530

    Priority Queues

--- Leetcode/Medium/215. Kth Largest Element in an Array.cpp
+++ Leetcode/Medium/215. Kth Largest Element in an Array.cpp
@@ -1,5 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
@@ -11,17 +13,17 @@ using namespace std;
 class Solution
 {
 public:
- int findKthLargest(vector<int> &nums, int k)
- {
-  priority_queue<int, vector<int>, greater<int>> pq;
-  for (int i = 0; i < nums.size(); i++)
-  {
-   pq.push(nums[i]);
-   if (pq.size() > k)
-   {
-    pq.pop();
-   }
-  }
-  return pq.top();
- }
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            pq.push(nums[i]);
+            if (pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
 };
--- Leetcode/Medium/347. Top K Frequent Elements.cpp
+++ Leetcode/Medium/347. Top K Frequent Elements.cpp
@@ -1,5 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
@@ -16,33 +18,33 @@ using namespace std;
 class Solution
 {
 public:
- vector<int> topKFrequent(vector<int> &nums, int k)
- {
-  unordered_map<int, int> freq;
-  for (int i = 0; i < nums.size(); i++)
-  {
-   freq[nums[i]]++;
-  }
-  // min priority queue
-  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-  vector<int> ans;
-  for (auto n : freq)
-  {
-   int num = n.first;
-   int num_freq = n.second;
-   pq.push(make_pair(num_freq, num));
-   // when size of pq exceeds k, we pop number with least freq
-   if (pq.size() > k)
-   {
-    pq.pop();
-   }
-  }
-  while (pq.size() != 0)
-  {
-   int num = (pq.top()).second;
-   ans.push_back(num);
-   pq.pop();
-  }
-  return ans;
- }
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> freq;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            freq[nums[i]]++;
+        }
+        // min priority queue
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> ans;
+        for (auto n : freq)
+        {
+            int num = n.first;
+            int num_freq = n.second;
+            pq.push(make_pair(num_freq, num));
+            // when size of pq exceeds k, we pop number with least freq
+            if (pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+        while (pq.size() != 0)
+        {
+            int num = (pq.top()).second;
+            ans.push_back(num);
+            pq.pop();
+        }
+        return ans;
+    }
 };
