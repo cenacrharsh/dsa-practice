121ef335a70d9e3186559b646e00463d6cb2b9ef
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-03-01 20:48:02 +0530

    226. Invert Binary Tree

--- Leetcode/Easy/226. Invert Binary Tree.cpp
+++ Leetcode/Easy/226. Invert Binary Tree.cpp
@@ -1,21 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
 #include <cstring>
 using namespace std;
 
-//# Tutorial: https://www.youtube.com/watch?v=fKgZiCXb6zs
+// # Tutorial: https://www.youtube.com/watch?v=fKgZiCXb6zs
 
 struct TreeNode
 {
- int val;
- TreeNode *left;
- TreeNode *right;
- TreeNode() : val(0), left(nullptr), right(nullptr) {}
- TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 //! Iterative
@@ -23,39 +25,39 @@ struct TreeNode
 class Solution
 {
 public:
- TreeNode *invertTree(TreeNode *root)
- {
-  if (root == nullptr)
-  {
-   return root;
-  }
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return root;
+        }
 
-  queue<TreeNode *> tree;
-  tree.push(root);
+        queue<TreeNode *> tree;
+        tree.push(root);
 
-  while (!tree.empty())
-  {
-   TreeNode *front = tree.front();
-   tree.pop();
+        while (!tree.empty())
+        {
+            TreeNode *front = tree.front();
+            tree.pop();
 
-   if (front->left != nullptr)
-   {
-    tree.push(front->left);
-   }
+            if (front->left != nullptr)
+            {
+                tree.push(front->left);
+            }
 
-   if (front->right != nullptr)
-   {
-    tree.push(front->right);
-   }
+            if (front->right != nullptr)
+            {
+                tree.push(front->right);
+            }
 
-   // swap
-   TreeNode *temp = front->left;
-   front->left = front->right;
-   front->right = temp;
-  }
+            // swap
+            TreeNode *temp = front->left;
+            front->left = front->right;
+            front->right = temp;
+        }
 
-  return root;
- }
+        return root;
+    }
 };
 
 //! Recursive
@@ -63,20 +65,20 @@ public:
 class Solution
 {
 public:
- TreeNode *invertTree(TreeNode *root)
- {
-  if (root == NULL)
-  {
-   return root;
-  }
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return root;
+        }
 
-  TreeNode *left = invertTree(root->left);
-  TreeNode *right = invertTree(root->right);
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
 
-  // swap
-  root->right = left;
-  root->left = right;
+        // swap
+        root->right = left;
+        root->left = right;
 
-  return root;
- }
+        return root;
+    }
 };
