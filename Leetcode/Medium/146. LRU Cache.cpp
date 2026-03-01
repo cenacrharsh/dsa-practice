57a3718f0ecc4f04217fc03c027bc789688bd3d3
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-03-01 18:13:13 +0530

    146. LRU Cache

--- Leetcode/Medium/146. LRU Cache.cpp
+++ Leetcode/Medium/146. LRU Cache.cpp
@@ -1,5 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 #include <algorithm>
 #include <climits>
 #include <unordered_map>
@@ -19,101 +21,101 @@ using namespace std;
 class Node
 {
 public:
-  int key;
-  int val;
-  Node *next;
-  Node *prev;
+    int key;
+    int val;
+    Node *next;
+    Node *prev;
 
-  Node(int key, int val)
-  {
-    this->key = key;
-    this->val = val;
-    this->next = NULL;
-    this->prev = NULL;
-  }
+    Node(int key, int val)
+    {
+        this->key = key;
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
 };
 
 class LRUCache
 {
 public:
-  int capacity;
-  Node *head;
-  Node *tail;
-  unordered_map<int, Node *> mp;
+    int capacity;
+    Node *head;
+    Node *tail;
+    unordered_map<int, Node *> mp;
 
-  LRUCache(int capacity)
-  {
-    this->capacity = capacity;
-    mp.clear();
-    head = new Node(-1, -1);
-    tail = new Node(-1, -1);
-    head->next = tail;
-    tail->prev = head;
-  }
-
-  void deleteNode(Node *node)
-  {
-    Node *prevNode = node->prev;
-    Node *nextNode = node->next;
-
-    prevNode->next = nextNode;
-    nextNode->prev = prevNode;
-  }
-
-  void insertAfterHead(Node *newNode)
-  {
-    Node *nextNode = head->next;
-
-    newNode->next = nextNode;
-    newNode->prev = head;
-
-    head->next = newNode;
-    nextNode->prev = newNode;
-  }
-
-  int get(int key)
-  {
-    if (mp.find(key) == mp.end())
+    LRUCache(int capacity)
     {
-      return -1;
+        this->capacity = capacity;
+        mp.clear();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head->next = tail;
+        tail->prev = head;
     }
 
-    Node *node = mp[key];
-
-    //* put the accessed key to the front of the list
-    deleteNode(node);
-    insertAfterHead(node);
-
-    return node->val;
-  }
-
-  void put(int key, int value)
-  {
-    //* if element is already present
-    if (mp.find(key) != mp.end())
+    void deleteNode(Node *node)
     {
-      Node *node = mp[key];
-      node->val = value;
+        Node *prevNode = node->prev;
+        Node *nextNode = node->next;
 
-      //* since it is recently accessed, put it in the front
-      deleteNode(node);
-      insertAfterHead(node);
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
     }
-    else
+
+    void insertAfterHead(Node *newNode)
     {
-      //* if size == capacity, remove least recently used element
-      if (mp.size() == capacity)
-      {
-        Node *node = tail->prev;
-        mp.erase(node->key);
+        Node *nextNode = head->next;
+
+        newNode->next = nextNode;
+        newNode->prev = head;
+
+        head->next = newNode;
+        nextNode->prev = newNode;
+    }
+
+    int get(int key)
+    {
+        if (mp.find(key) == mp.end())
+        {
+            return -1;
+        }
+
+        Node *node = mp[key];
+
+        //* put the accessed key to the front of the list
         deleteNode(node);
-      }
+        insertAfterHead(node);
 
-      Node *newNode = new Node(key, value);
-      mp[key] = newNode;
-      insertAfterHead(newNode);
+        return node->val;
     }
-  }
+
+    void put(int key, int value)
+    {
+        //* if element is already present
+        if (mp.find(key) != mp.end())
+        {
+            Node *node = mp[key];
+            node->val = value;
+
+            //* since it is recently accessed, put it in the front
+            deleteNode(node);
+            insertAfterHead(node);
+        }
+        else
+        {
+            //* if size == capacity, remove least recently used element
+            if (mp.size() == capacity)
+            {
+                Node *node = tail->prev;
+                mp.erase(node->key);
+                deleteNode(node);
+            }
+
+            Node *newNode = new Node(key, value);
+            mp[key] = newNode;
+            insertAfterHead(newNode);
+        }
+    }
 };
 
 /**
