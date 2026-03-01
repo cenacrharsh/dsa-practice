9ab94640af159e34eccd4b7cd946e826c7b1ad3c
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2026-03-01 15:32:17 +0530

    496. Next Greater Element I

--- Leetcode/Easy/496. Next Greater Element I.cpp
+++ Leetcode/Easy/496. Next Greater Element I.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
 #include <queue>
 #include <stack>
@@ -20,32 +20,32 @@ using namespace std;
 class Solution
 {
 public:
-  vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
-  {
-    stack<int> st;
-    unordered_map<int, int> mp;
-    for (int i = nums2.size() - 1; i >= 0; i--)
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
     {
-      while (!st.empty() && st.top() <= nums2[i])
-      {
-        st.pop();
-      }
-      if (st.empty())
-      {
-        mp[nums2[i]] = -1;
-      }
-      else
-      {
-        mp[nums2[i]] = st.top();
-      }
-      st.push(nums2[i]);
-    }
+        stack<int> st;
+        unordered_map<int, int> mp;
+        for (int i = nums2.size() - 1; i >= 0; i--)
+        {
+            while (!st.empty() && st.top() <= nums2[i])
+            {
+                st.pop();
+            }
+            if (st.empty())
+            {
+                mp[nums2[i]] = -1;
+            }
+            else
+            {
+                mp[nums2[i]] = st.top();
+            }
+            st.push(nums2[i]);
+        }
 
-    vector<int> ans(nums1.size(), 0);
-    for (int i = 0; i < nums1.size(); i++)
-    {
-      ans[i] = mp[nums1[i]];
+        vector<int> ans(nums1.size(), 0);
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            ans[i] = mp[nums1[i]];
+        }
+        return ans;
     }
-    return ans;
-  }
 };
